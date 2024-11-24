4c8c42e6ff72eb65b10c2ea1f71c8c84cdf91e10
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-11-24 13:16:07 +0900

    add utils

---
+++ apps/job/src/utils/database/prisma.ts
@@ -0,0 +1,27 @@
+import { PrismaClient, getDbUrl } from "@repo/db-job";
+import { getEnv } from "../env";
+
+export const getSingleTenantPrismaClient = () => {
+  const {
+    APPS_DB_NAME,
+    APPS_DB_PORT,
+    APPS_DB_USER_RLS_ENABLED,
+    APPS_DB_PASS_RLS_ENABLED,
+  } = getEnv();
+
+  const dbUrl = getDbUrl({
+    DB_NAME: APPS_DB_NAME,
+    DB_PORT: APPS_DB_PORT,
+    DB_USER: APPS_DB_USER_RLS_ENABLED,
+    DB_PASS: APPS_DB_PASS_RLS_ENABLED,
+    CONNECTION_LIMIT: 1,
+  });
+
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: dbUrl,
+      },
+    },
+  });
+};
