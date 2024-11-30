96f523d666d4a05469618f049323c1b92da7f6c2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-11-30 23:36:19 +0900

    update routes

--- apps/web-remix-v3/app/routes.ts
+++ apps/web-remix-v3/app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+
+export default [
+  route("login", "routes/login/page.tsx"),
+
+  // dashboard layout
+  layout("routes/dashboard/layout.tsx", [
+    route("/", "routes/dashboard/index.tsx"),
+    route("/:teamId", "routes/dashboard/$teamId/index.tsx"),
+  ]),
+] satisfies RouteConfig;
