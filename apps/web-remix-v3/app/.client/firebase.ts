8cf1cfa3583e6371bdabed7584f979778c38f6df
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-01 20:32:54 +0900

    feat: firebase auth (#63)

---
+++ apps/web-remix-v3/app/.client/firebase.ts
@@ -0,0 +1,12 @@
+import { type FirebaseOptions, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { publicViteEnv } from "~/env";
+
+const firebaseConfigsBrowser = {
+  projectId: publicViteEnv.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: publicViteEnv.VITE_PUBLIC_FIREBASE_BROWSER_API_KEY,
+} satisfies FirebaseOptions;
+
+const firebaseAppBrowser = initializeApp(firebaseConfigsBrowser);
+
+export const auth = getAuth(firebaseAppBrowser);
--- apps/web-remix-v3/app/routes/dashboard/$teamId/index.tsx
+++ apps/web-remix-v3/app/routes/dashboard/$teamId/index.tsx
@@ -1,3 +1,4 @@
+import { auth } from "~/.client";
 import type { Route } from "../../dashboard/$teamId/+types/index";
 
 // biome-ignore lint: remix default setup
@@ -8,15 +9,34 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  const isNotAuthorized = true;
+type DashboardData = {
+  panelPRs: {
+    count: number;
+  }[];
+};
 
-  const { teamId } = params;
-  if (teamId !== "demo" && isNotAuthorized) {
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  await auth.authStateReady();
+
+  if (params.teamId === "demo") {
+    return {
+      panelPRs: [{ count: 10 }],
+    } satisfies DashboardData;
+  }
+
+  if (!auth.currentUser) {
     location.href = "/login";
   }
+
+  // NOTE: fetch data from server
+  // const data = await fetch(`/api/teams/${params.teamId}/dashboard`);
+  // return data satisfies DashboardData;
+  return {
+    panelPRs: [{ count: 0 }],
+  };
 }
 
-export default function Home() {
-  return <div>teamId</div>;
+export default function Page({ loaderData }: Route.ComponentProps) {
+  const { panelPRs } = loaderData;
+  return <>panelPRs: {panelPRs[0].count}</>;
 }
