f63acae011874ae24a16e2bcea6170d3275c7ea0
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-02 00:06:16 +0900

    feat: add sidebar

--- apps/web/app/routes/dashboard/layout.tsx
+++ apps/web/app/routes/dashboard/layout.tsx
@@ -1,9 +1,10 @@
+import { SidebarProject } from "@repo/ui/SidebarProject";
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <div className="flex">
-      <div className="h-screen  w-36 bg-amber-50">Sidebar</div>
+      <SidebarProject projectId="demo" />
       <div>
         <Outlet />
       </div>
