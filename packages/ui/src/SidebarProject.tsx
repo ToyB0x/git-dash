a89e27068be33e2d9204658427e89a41fcff7ffc
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-02 00:07:01 +0900

    feat: add sidebar (#76)

---
+++ packages/ui/src/SidebarProject.tsx
@@ -0,0 +1,45 @@
+import { Box } from "@chakra-ui/react";
+import type { FC } from "react";
+import { Link } from "react-router";
+
+type LinkItem = {
+  name: string;
+  href: string;
+};
+
+const linkItems: LinkItem[] = [
+  { name: "Dashboard", href: "" },
+  { name: "PRs", href: "prs" },
+  { name: "Reviews", href: "reviews" },
+];
+
+type Props = {
+  projectId: string;
+};
+
+export const SidebarProject: FC<Props> = ({ projectId }) => {
+  return (
+    <Box
+      w="12rem"
+      pos="sticky"
+      py={2}
+      px={4}
+      borderRight="1px"
+      borderRightColor="gray.200"
+    >
+      <Link to="/">
+        <Box fontSize="4xl" fontWeight="bold" fontFamily="monospace">
+          git-dash
+        </Box>
+      </Link>
+
+      {linkItems.map((link) => (
+        <Link to={`${projectId}/${link.href}`} key={link.name}>
+          <Box ml={1} my={2} display="block" fontSize="lg">
+            {link.name}
+          </Box>
+        </Link>
+      ))}
+    </Box>
+  );
+};
