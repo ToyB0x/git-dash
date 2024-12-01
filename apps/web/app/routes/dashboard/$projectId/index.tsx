7a0ff2fe35bdec9d8b4fddec22d6722c8d06be25
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-02 00:30:15 +0900

    use StatCard

--- apps/web/app/routes/dashboard/$projectId/index.tsx
+++ apps/web/app/routes/dashboard/$projectId/index.tsx
@@ -1,3 +1,9 @@
+import { StatCard } from "@repo/ui/StatCard";
+import { BsStar } from "react-icons/bs";
+import { GiBiohazard, GiSandsOfTime } from "react-icons/gi";
+import { GoCommentDiscussion } from "react-icons/go";
+import { IoIosGitPullRequest } from "react-icons/io";
+import { SlSpeedometer } from "react-icons/sl";
 import { auth } from "~/.client";
 import type { Route } from "../../dashboard/$projectId/+types/index";
 
@@ -9,17 +15,27 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-type DashboardData = {
-  panelPRs: {
-    count: number;
-  }[];
+type DashboardDataV0 = {
+  statCards: {
+    mergedCount: number;
+    reviewCount: number;
+    waitingCount: number;
+    releaseCount: number;
+    vulnCount: number;
+  };
 };
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   if (params.projectId === "demo") {
     return {
-      panelPRs: [{ count: 10 }],
-    } satisfies DashboardData;
+      statCards: {
+        mergedCount: 10,
+        reviewCount: 10,
+        waitingCount: 10,
+        releaseCount: 10,
+        vulnCount: 10,
+      },
+    } satisfies DashboardDataV0;
   }
 
   await auth.authStateReady();
@@ -28,15 +44,64 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
     location.href = "/login";
   }
 
-  // NOTE: fetch data from server
-  // const data = await fetch(`/api/teams/${params.projectId}/dashboard`);
-  // return data satisfies DashboardData;
+  // TODO: fetch data from server
   return {
-    panelPRs: [{ count: 0 }],
-  };
+    statCards: {
+      mergedCount: 10,
+      reviewCount: 10,
+      waitingCount: 10,
+      releaseCount: 10,
+      vulnCount: 10,
+    },
+  } satisfies DashboardDataV0;
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
-  const { panelPRs } = loaderData;
-  return <>panelPRs: {panelPRs[0].count}</>;
+  const { statCards } = loaderData;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr",
+        gap: "1%",
+        height: "100px",
+      }}
+    >
+      <StatCard
+        title="マージ済みPR"
+        stat={`${statCards.mergedCount}/month`}
+        icon={<IoIosGitPullRequest size="3rem" />}
+      />
+
+      <StatCard
+        title="マージ速度"
+        stat={`${Math.round(statCards.mergedCount / (30 - 8))}/day`}
+        icon={<SlSpeedometer size="3rem" />}
+      />
+
+      <StatCard
+        title="レビュー数"
+        stat={`${statCards.reviewCount}/month`}
+        icon={<GoCommentDiscussion size="3rem" />}
+      />
+
+      <StatCard
+        title="レビュー待ちPR"
+        stat={statCards.waitingCount.toString()}
+        icon={<GiSandsOfTime size="3rem" />}
+      />
+
+      <StatCard
+        title="リリース数"
+        stat={`${statCards.releaseCount}/month`}
+        icon={<BsStar size="3rem" />}
+      />
+
+      <StatCard
+        title="脆弱なパッケージ"
+        stat={statCards.vulnCount.toString()}
+        icon={<GiBiohazard size="3rem" />}
+      />
+    </div>
+  );
 }
