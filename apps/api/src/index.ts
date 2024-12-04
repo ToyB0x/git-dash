5916e1ab4abf00271c1d49a3b4be9af18610d764
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-05 03:12:40 +0900

    feat: add R2 PUT code (#110)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono! deployed by github actions");
+});
+
+app.get("/report", async (c) => {
+  const res = await c.env.REPORT_BUCKET.put(
+    "key-1",
+    JSON.stringify({ key1: "value-1" }),
+  );
+  console.info(res);
+  return c.text("ok");
 });
 
 export default app;
