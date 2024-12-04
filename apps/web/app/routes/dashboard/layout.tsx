f4db3b0bf5256fd323723eed2d1e76ab1ad045b2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-05 01:18:36 +0900

    fix: redirect flash screen (#107)

--- apps/web/app/routes/dashboard/layout.tsx
+++ apps/web/app/routes/dashboard/layout.tsx
@@ -1,7 +1,23 @@
 import { SidebarProject } from "@repo/ui/SidebarProject";
 import { Outlet } from "react-router";
+import { auth } from "~/.client";
+import type { Route } from "../dashboard/+types/layout";
 
-export default function Layout() {
+export async function clientLoader() {
+  await auth.authStateReady();
+  if (!auth.currentUser && location.pathname !== "/demo") {
+    location.href = "/login";
+    return { isLoading: true };
+  }
+
+  return { isLoading: false };
+}
+
+export default function Layout({ loaderData }: Route.ComponentProps) {
+  // NOTE: prevent flash on redirecting to login page
+  const { isLoading } = loaderData;
+  if (isLoading) return null;
+
   return (
     <div className="flex">
       <SidebarProject projectId="demo" />
