4bc8aacbe7ca7ebc75b9fb2a6819c2b9c74223c9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-06 23:15:39 +0900

    fix endpoint

--- apps/api/src/routes/reports/index.ts
+++ apps/api/src/routes/reports/index.ts
@@ -1,4 +1,4 @@
 import { Hono } from "hono";
 import { postHandler } from "./post";
 
-export const reportRoute = new Hono().post("/post", ...postHandler); // URLからはTypeの推測ができないためParse処理が重くなる可能性がある(その場合は /:type などでパスにTypeも含めてTypeごとにParse処理を切り替える)
+export const reportRoute = new Hono().post("", ...postHandler); // URLからはTypeの推測ができないためParse処理が重くなる可能性がある(その場合は /:type などでパスにTypeも含めてTypeごとにParse処理を切り替える)
