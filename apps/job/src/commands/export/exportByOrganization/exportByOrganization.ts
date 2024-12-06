275229800b2ace398e607dac813be944868b7735
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-06 23:16:04 +0900

    update job

--- apps/job/src/commands/export/exportByOrganization/exportByOrganization.ts
+++ apps/job/src/commands/export/exportByOrganization/exportByOrganization.ts
@@ -1,21 +1,26 @@
+import type { AppType } from "@repo/api";
+import { hc } from "hono/client";
 import { getSingleTenantPrismaClient } from "../../../utils";
 import { countOnce } from "./statCardMerged";
 
+// TODO: split to /client
+// TODO: use url from Env
+const client = hc<AppType>("http://localhost:8787/");
+
 export const exportByOrganization = async (orgName: string): Promise<void> => {
+  const resHC = await client["reports-meta"].$post();
+  if (!resHC.ok) throw Error("Failed to create report");
+
+  const { id: reportId } = await resHC.json();
+
   const prisma = getSingleTenantPrismaClient();
   const { id: organizationId } = await prisma.organization.findUniqueOrThrow({
     where: { login: orgName },
   });
 
-  const mergedCount = await countOnce(organizationId, 90);
+  const mergedCount = await countOnce(organizationId, reportId, 90);
+
   console.log(JSON.stringify(mergedCount, null, 2));
 
-  // TODO: use url from Env
-  const res = await fetch("http://localhost:8787/reports", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(mergedCount),
-  });
-
-  console.log(await res.json());
+  await client.reports.$post({ json: mergedCount });
 };
