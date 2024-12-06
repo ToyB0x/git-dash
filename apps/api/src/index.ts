5bd8f54e2bddf626e897306c7330538a79799e33
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-06 20:21:43 +0900

    update index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
-import { reportRoute } from "./routes";
+import { reportMetaRoute, reportRoute } from "./routes";
 
 const app = new Hono();
-const routes = app.route("/reports", reportRoute);
+const routes = app
+  .route("/reports", reportRoute)
+  .route("/reports-meta", reportMetaRoute);
 
 export default app;
 export type AppType = typeof routes;
