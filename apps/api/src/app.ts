afe9341545abd914b8164dda3620a21ed8b837ac
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-07 16:45:27 +0900

    refactor hono route

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { reportMetaRoute, reportRoute } from "./routes";
 
-const app = new Hono();
-const routes = app
+export const app = new Hono()
+  .use(
+    "/*",
+    cors({
+      origin: ["http://localhost:10000"],
+    }),
+  )
   .route("/reports", reportRoute)
   .route("/reports-meta", reportMetaRoute);
-
-export default app;
-export type AppType = typeof routes;
