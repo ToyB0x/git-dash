4e9f2984b4eece1be8f142227a39461220a292d4
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-07 16:53:12 +0900

    update job

--- apps/job/src/commands/export/exportByOrganization/exportByOrganization.ts
+++ apps/job/src/commands/export/exportByOrganization/exportByOrganization.ts
@@ -1,17 +1,12 @@
-import type { AppType } from "@repo/api";
-import { hc } from "hono/client";
+import { client } from "../../../clients/hono";
 import { getSingleTenantPrismaClient } from "../../../utils";
 import { countOnce } from "./statCardMerged";
 
-// TODO: split to /client
-// TODO: use url from Env
-const client = hc<AppType>("http://localhost:8787/");
-
 export const exportByOrganization = async (orgName: string): Promise<void> => {
-  const resHC = await client["reports-meta"].$post();
-  if (!resHC.ok) throw Error("Failed to create report");
+  const resPostMeta = await client["reports-meta"].$post();
+  if (!resPostMeta.ok) throw Error("Failed to create report");
 
-  const { id: reportId } = await resHC.json();
+  const { id: reportId } = await resPostMeta.json();
 
   const prisma = getSingleTenantPrismaClient();
   const { id: organizationId } = await prisma.organization.findUniqueOrThrow({
