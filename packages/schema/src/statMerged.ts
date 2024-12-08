c82907d453e6134fff428fb6d89517c9e1bc03a2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-09 01:52:05 +0900

    rename schema

--- packages/schema/src/statMerged.ts
+++ packages/schema/src/statMerged.ts
@@ -4,7 +4,7 @@ const typeName = "statMerged";
 
 const schema = v.object({
   reportId: v.string(),
-  teamId: v.pipe(v.string(), v.uuid()),
+  groupId: v.pipe(v.string(), v.uuid()),
   type: v.literal(typeName),
   version: v.literal("1.0"), // latest version
   data: v.array(
@@ -16,7 +16,7 @@ const schema = v.object({
 });
 
 const fixture = {
-  teamId: "2edd4c47-b01c-49eb-9711-5e8106bbabcf",
+  groupId: "2edd4c47-b01c-49eb-9711-5e8106bbabcf",
   reportId: "1",
   type: "statMerged",
   version: "1.0",
