7d13d3b59d62d1ba070d7776803c6c90ac34533b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-09 01:41:35 +0900

    add team route

---
+++ apps/api/src/routes/teams/get.ts
@@ -0,0 +1,30 @@
+import { getFirebaseToken } from "@hono/firebase-auth";
+import { groupTbl, userTbl, usersToGroups } from "@repo/db-api/schema";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { createFactory } from "hono/factory";
+
+const factory = createFactory<{ Bindings: Env }>();
+
+const handlers = factory.createHandlers(async (c) => {
+  const idToken = getFirebaseToken(c);
+  if (!idToken) throw Error("Unauthorized");
+
+  const db = drizzle(c.env.DB_API);
+
+  const belongingTeams = await db
+    .select({ publicId: groupTbl.publicId, displayName: groupTbl.displayName })
+    .from(usersToGroups)
+    .leftJoin(userTbl, eq(usersToGroups.userId, userTbl.id))
+    .leftJoin(groupTbl, eq(usersToGroups.groupId, groupTbl.id))
+    .where(eq(userTbl.firebaseUid, idToken.uid));
+
+  return c.json(
+    belongingTeams.filter(
+      (team): team is { publicId: string; displayName: string } =>
+        !team.publicId || !team.displayName,
+    ),
+  );
+});
+
+export const getHandler = handlers;
