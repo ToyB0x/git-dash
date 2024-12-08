f2b85a95f19fb21075c972ff84d4d83068b446cb
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-08 11:05:02 +0900

    refactor: fetcher (#123)

--- packages/schema/src/path.ts
+++ packages/schema/src/path.ts
@@ -1,8 +1,7 @@
-// teams/{teamId}/reports/{reportId}/types/{type}/versions/{version}/data.json
+// teams/{teamId}/reports/{reportId}/types/{type}/data.json
 export const getR2Path = ({
   teamId,
   reportId,
   type,
-  version,
 }: { teamId: string; reportId: number; type: string; version: string }) =>
-  `teams/${teamId}/reports/${reportId}/types/${type}/versions/${version}/data.json`;
+  `teams/${teamId}/reports/${reportId}/types/${type}/data.json`;
