3f8019a1f0499c24ba63b274f944fe6122b98095
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-09 01:39:32 +0900

    add idxGroupId relation to report

--- packages/db-api/src/schema/reportTbl.ts
+++ packages/db-api/src/schema/reportTbl.ts
@@ -1,5 +1,6 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { customAlphabet } from "nanoid";
+import { groupTbl } from "./groupTbl";
 
 // NOTE: same as Planet scale's choice
 // ~919 years or 8B IDs needed, in order to have a 1% probability of at least one collision
@@ -8,17 +9,29 @@ const idLength = 12;
 const idAlphabet =
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-export const reportTbl = sqliteTable("report", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: false }),
-  publicId: text("public_id", { length: idLength })
-    .unique("uq_report_public_id")
-    .notNull(),
-  status: text({
-    enum: ["RUNNING", "FINISHED", "ABORTED", "FAILED"],
-  }).notNull(),
-  createdAt: integer({ mode: "timestamp_ms" }).notNull(),
-  updatedAt: integer({ mode: "timestamp_ms" }).notNull(),
-});
+export const reportTbl = sqliteTable(
+  "report",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: false }),
+    publicId: text("public_id", { length: idLength })
+      .unique("uq_report_public_id")
+      .notNull(),
+    status: text({
+      enum: ["RUNNING", "FINISHED", "ABORTED", "FAILED"],
+    }).notNull(),
+    createdAt: integer({ mode: "timestamp_ms" }).notNull(),
+    updatedAt: integer({ mode: "timestamp_ms" }).notNull(),
+    groupId: integer("group_id", { mode: "number" })
+      .notNull()
+      .references(() => groupTbl.id, {
+        onUpdate: "cascade",
+        onDelete: "cascade",
+      }),
+  },
+  (tbl) => ({
+    idxGroupId: index("idx_report_group_id").on(tbl.groupId),
+  }),
+);
 
 export const generateNewReportId = (): string => {
   const nanoid = customAlphabet(idAlphabet, idLength);
