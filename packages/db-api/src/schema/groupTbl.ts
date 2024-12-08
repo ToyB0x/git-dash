452c1b0b1fead0dbdf141e3ad173d9465a26d1d9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-08 17:05:49 +0900

    add group table

---
+++ packages/db-api/src/schema/groupTbl.ts
@@ -0,0 +1,25 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { customAlphabet } from "nanoid";
+
+const idLength = 8;
+const idAlphabet =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+export const groupTbl = sqliteTable("group", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: false }),
+  publicId: text("public_id", { length: idLength }).unique(
+    "uq_group_public_id",
+  ),
+  displayName: text({ length: 24 }).notNull(),
+  createdAt: integer({ mode: "timestamp_ms" })
+    .notNull()
+    .$default(() => new Date()),
+  updatedAt: integer({ mode: "timestamp_ms" })
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const generateNewGroupId = (): string => {
+  const nanoid = customAlphabet(idAlphabet, idLength);
+  return nanoid(); //=> "UPsnynTq"
+};
