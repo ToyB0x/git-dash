2cb3fd7fb79ca5b633ef41d83a8e9b5188bd661d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-08 15:35:54 +0900

    add nano id generator

--- packages/db-api/src/schema/userTbl.ts
+++ packages/db-api/src/schema/userTbl.ts
@@ -6,6 +6,7 @@ import {
   text,
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
+import { customAlphabet } from "nanoid";
 
 export const userTbl = sqliteTable(
   "user",
@@ -31,3 +32,10 @@ export const userTbl = sqliteTable(
 export function lower(column: AnySQLiteColumn): SQL {
   return sql`lower(${column})`;
 }
+
+export const generateNewUserId = (): string => {
+  const alphabet =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nanoid = customAlphabet(alphabet, 8);
+  return nanoid(); //=> "UPsnynTq"
+};
