887428e40b4f7386cc1af38b0a257145308d46dc
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-08 16:08:42 +0900

    refactor: update table primary key with nano id (#129)

--- packages/db-api/drizzle/0000_bitter_khan.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `report` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`status` text NOT NULL,
-	`createdAt` integer NOT NULL,
-	`updatedAt` integer NOT NULL
-);
--- packages/db-api/src/schema/reportTbl.ts
+++ packages/db-api/src/schema/reportTbl.ts
@@ -1,10 +1,23 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { customAlphabet } from "nanoid";
+
+// NOTE: same as Planet scale's choice
+// ~919 years or 8B IDs needed, in order to have a 1% probability of at least one collision
+// ref: https://planetscale.com/blog/why-we-chose-nanoids-for-planetscales-api
+const idLength = 12;
+const idAlphabet =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 export const reportTbl = sqliteTable("report", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: false }),
+  id: text({ length: idLength }).primaryKey(),
   status: text({
     enum: ["RUNNING", "FINISHED", "ABORTED", "FAILED"],
   }).notNull(),
   createdAt: integer({ mode: "timestamp_ms" }).notNull(),
   updatedAt: integer({ mode: "timestamp_ms" }).notNull(),
 });
+
+export const generateNewReportId = (): string => {
+  const nanoid = customAlphabet(idAlphabet, idLength);
+  return nanoid(); //=> "MSZTcPfZXoEN"
+};
