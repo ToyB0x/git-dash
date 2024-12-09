d0420cfb38dd7699be27c4d6222c819ae5dc3902
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-09 13:05:28 +0900

    update client config

--- apps/web/app/.client/hono.ts
+++ apps/web/app/.client/hono.ts
@@ -1,7 +1,8 @@
 import { hcWithType } from "@repo/api/hc";
+import { publicViteEnv } from "~/env";
 
 // TODO: use url from Env
-export const client = hcWithType("http://localhost:8787", {
+export const client = hcWithType(publicViteEnv.VITE_PUBLIC_API_SERVER_URL, {
   // TODO: add header for jwt
   init: {},
 });
