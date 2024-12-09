1fe929657d73894305804b95865f095281ab8fef
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-10 06:43:48 +0900

    add env check command

--- apps/job/src/commands/export/commands.ts
+++ apps/job/src/commands/export/commands.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import { Command, Option } from "commander";
 import { exportByOrganization } from "./exportByOrganization";
 
@@ -31,6 +32,35 @@ export const makeExportCommand = () => {
         );
 
       await exportByOrganization(githubOrganizationName, groupId, groupApiKey);
+    });
+
+  exportCmd
+    .command("check")
+    .description("check command envs")
+    .addOption(
+      new Option(
+        "--githubOrganizationName <string>",
+        "Target Github Organization",
+      ).env("GDASH_GITHUB_ORGANIZATION_NAME"),
+    )
+    .addOption(
+      new Option("--groupId <string>", "G-dash Group ID").env("GDASH_GROUP_ID"),
+    )
+    .addOption(
+      new Option("--groupApiKey <string>", "G-dash Group API Key").env(
+        "GDASH_GROUP_API_KEY",
+      ),
+    )
+    .action(async (options) => {
+      const { githubOrganizationName, groupId, groupApiKey } = options;
+
+      if (!githubOrganizationName || !groupId || !groupApiKey)
+        throw new Error(
+          "Invalid arguments, confirm githubOrganizationName, groupId, and groupApiKey are set",
+        );
+
+      console.info("envs are set correctly");
+      process.exit(0);
     });
 
   return exportCmd;
