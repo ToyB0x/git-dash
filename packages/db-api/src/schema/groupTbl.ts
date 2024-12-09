dd2b5167de51c52263e0a609ee031be6ea382ca4
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-10 04:48:29 +0900

    feat: add api token column

--- packages/db-api/src/schema/groupTbl.ts
+++ packages/db-api/src/schema/groupTbl.ts
@@ -1,13 +1,19 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { customAlphabet } from "nanoid";
+import { customAlphabet, nanoid } from "nanoid";
 
 const idLength = 8;
 const idAlphabet =
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
+const generateNewApiToken = (): string => nanoid(32);
+
 export const groupTbl = sqliteTable("group", {
   id: text({ length: idLength }).primaryKey(),
   displayName: text({ length: 24 }).notNull(),
+  // TODO: トークンをデフォルトで作らないようにする /　トークンの作成, 削除機能を作る
+  apiToken: text("api_token", { length: 32 })
+    .notNull()
+    .$default(generateNewApiToken),
   createdAt: integer({ mode: "timestamp_ms" })
     .notNull()
     .$default(() => new Date()),
