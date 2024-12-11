76a8db617774a1312595ebabc59444a6fbce4b03
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-11 10:18:17 +0900

    feat: add statReviews

---
+++ packages/schema/src/statReviews.ts
@@ -0,0 +1,36 @@
+import * as v from "valibot";
+
+const version = "1.0";
+const typeName = "statReviews";
+
+const schema = v.object({
+  reportId: v.string(),
+  groupId: v.string(),
+  type: v.literal(typeName),
+  version: v.literal(version), // latest version
+  data: v.array(
+    v.object({
+      login: v.string(),
+      count: v.number(),
+    }),
+  ),
+});
+
+const fixture = {
+  groupId: "2edd4c47-b01c-49eb-9711-5e8106bbabcf",
+  reportId: "1",
+  type: typeName,
+  version: version,
+  data: [
+    { login: "user1", count: 5 },
+    { login: "user2", count: 7 },
+  ],
+} satisfies Schema;
+
+export const stat = {
+  type: typeName,
+  schema,
+  fixture,
+};
+
+export type Schema = v.InferInput<typeof schema>;
