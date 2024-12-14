1b78100806ed3e1c938d10ff0611f98c4d7900fa
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-14 17:23:28 +0900

    add index page login check

--- apps/web/app/routes/(login)/index.tsx
+++ apps/web/app/routes/(login)/index.tsx
@@ -1,3 +1,13 @@
+import { auth } from "@/clients";
+import { redirect } from "react-router";
+
+export async function clientLoader() {
+  await auth.authStateReady();
+  if (!auth.currentUser) {
+    throw redirect("/sign-in");
+  }
+}
+
 export default function Page() {
   return <div>index</div>;
 }
