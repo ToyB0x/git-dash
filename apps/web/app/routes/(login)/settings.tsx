abd750cd69daad2f0e340fd3e7f8364c6ec0bdce
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-14 16:25:54 +0900

    add root settings page

---
+++ apps/web/app/routes/(login)/settings.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@/clients";
+import { redirect } from "react-router";
+
+export function meta() {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export async function clientLoader() {
+  // Root Settings ページはDemoユーザーであってもリダイレクトする (デモユーザの場合はRoot Settingsページへのリンクも非活性化しておく)
+  await auth.authStateReady();
+  if (!auth.currentUser) {
+    throw redirect("/login");
+  }
+}
+
+export default function Home() {
+  return <div className="dark:text-white">123</div>;
+}
