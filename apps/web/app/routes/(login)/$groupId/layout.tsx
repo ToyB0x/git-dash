bce94e81431b591f77c7055aaaacb063dfc0f208
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-14 21:45:36 +0900

    split groupId route

--- apps/web/app/routes/(login)/$groupId/layout.tsx
+++ apps/web/app/routes/(login)/$groupId/layout.tsx
@@ -1,14 +1,40 @@
 import { auth } from "@/clients";
-import { Outlet, redirect } from "react-router";
+import { Sidebar } from "@/components/ui/navigation/sidebar";
+import { Outlet, redirect, useLoaderData } from "react-router";
 import type { Route } from "../../../../.react-router/types/app/routes/(login)/$groupId/+types/layout";
 
+type LoginLayoutData = {
+  me: {
+    email: string;
+  };
+};
+
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  // layoutルートではparamsを扱いにくいため、paramsが絡むリダイレクトはlayoutファイルでは行わない
   await auth.authStateReady();
   if (!auth.currentUser && params.groupId !== "demo") {
     throw redirect("/sign-in");
   }
+
+  return {
+    me: {
+      email: auth.currentUser?.email ?? "demo@example.com",
+    },
+  } satisfies LoginLayoutData;
 }
 
 export default function Layout() {
-  return <Outlet />;
+  const { me } = useLoaderData<typeof clientLoader>();
+  return (
+    <div className="mx-auto max-w-screen-2xl">
+      <Sidebar email={me.email} />
+      <main className="lg:pl-72">
+        <div className="relative">
+          <div className="p-4 sm:px-6 sm:pb-10 sm:pt-10 lg:px-10 lg:pt-7">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
