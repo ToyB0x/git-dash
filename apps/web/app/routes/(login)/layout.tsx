2fb143878e7111078cafbfaaf83a774bab8f3639
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-14 14:56:17 +0900

    feat: add login banner as cover (#194)

--- apps/web/app/routes/(login)/layout.tsx
+++ apps/web/app/routes/(login)/layout.tsx
@@ -13,21 +13,14 @@ export default function layout() {
   );
 }
 
-// TODO: update banner form tremor repository sample image to custom image
-import banner from "./img.png";
-
 function Right() {
   return (
     <>
-      <div className="flex min-h-screen flex-1 flex-col justify-center px-4 lg:px-6 text-gray-200">
-        <div className="p-8">
-          <h3 className="text-center text-lg font-semibold ">Live Demo</h3>
-          <img
-            src={banner}
-            alt="dashboard sample"
-            className="w-[100%] mt-2 border-gray-400 border rounded-lg"
-          />
-          <p className="mt-4 text-xs">
+      {/*TODO: update banner form tremor repository sample image to custom image*/}
+      <div className="bg-cover bg-center bg-no-repeat bg-[url('/login-banner.png')]">
+        <div className="h-screen backdrop-blur-sm backdrop-brightness-50 flex justify-center flex-col text-center text-gray-100">
+          <h3 className=" text-lg font-semibold">Live Demo</h3>
+          <p className="mt-4 text-sm ">
             You can try the live demo with the
             <a href="/demo" className="underline underline-offset-4 pl-1">
               Demo Page
