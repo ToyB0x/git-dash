a17f052d7fd99258c46339711d9e69d63a3c1754
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-16 05:49:00 +0900

    feat: add user table (#228)

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -14,6 +14,7 @@ export default [
 
   // login state layout (group)
   route(":groupId", "routes/(login)/$groupId/layout.tsx", [
+    // コミットグラフ: 組織横断のコミットグラフ
     // route("", "routes/(login)/$groupId/overview/page.tsx"),
     // 分析バー: マージまでの時間の平均の階層をCategoryBarCardで表示？
     // 分析バー: レビューの平均待ち時間をCategoryBarCardで表示？
@@ -34,11 +35,11 @@ export default [
     // グラフ: リポジトリごとのVulnsの危険度がCRITICALの前月比グラフ (Overviewページには全リポジトリ横断のPRを表示)
     // リスト: リポジトリごとのVulnsの最新の5件
     // リスト: リポジトリごとのVulnsの危険度順の5件
-    // route("vulns", "routes/(login)/$groupId/vuln/page.tsx"),
+    route("vulns", "routes/(login)/$groupId/vuln/page.tsx"),
     // グラフ: 任意のユーザのコミットグラフ (今月分 + 前月分)
     // グラフ: 任意のユーザのPRグラフ (前月比グラフ)
     // グラフ: 任意のユーザのReviewグラフ (前月比グラフ)
-    // route("users", "routes/(login)/$groupId/user/page.tsx"),
+    route("users", "routes/(login)/$groupId/user/page.tsx"),
     route("home", "routes/(login)/$groupId/home.tsx"),
   ]),
 
