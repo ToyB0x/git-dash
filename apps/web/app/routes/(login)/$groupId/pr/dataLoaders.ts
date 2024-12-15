57d45b2e706e4c4b543ee1a7d5893f5c3362d548
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-15 16:59:34 +0900

    feat: add PRs chart

--- apps/web/app/routes/(login)/$groupId/pr/dataLoaders.ts
+++ apps/web/app/routes/(login)/$groupId/pr/dataLoaders.ts
@@ -2,65 +2,46 @@ import { generateDailyData } from "@/lib/generateDailyData";
 
 type GraphData = {
   version: "0.1";
-  type: "Actions2Core" | "Actions4Core" | "Actions16Core";
+  type: "PrOpen" | "PrMerge";
   data: {
     date: Date;
     value: number;
   }[];
 };
 
-export const dataLoaderActions2Core = async (
+export const dataLoaderPrOpen = async (
   _isDemo: boolean,
 ): Promise<GraphData> => {
   return {
-    type: "Actions2Core",
+    type: "PrOpen",
     version: "0.1",
     data: generateDailyData({
       startDate: new Date(
         Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
       ),
       endDate: new Date(),
-      min: 12,
-      max: 50,
-      variance: 0.04,
-      weekendReduction: false,
-    }),
-  };
-};
-
-export const dataLoaderActions4Core = async (
-  _isDemo: boolean,
-): Promise<GraphData> => {
-  return {
-    type: "Actions4Core",
-    version: "0.1",
-    data: generateDailyData({
-      startDate: new Date(
-        Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
-      ),
-      endDate: new Date(),
-      min: 20,
-      max: 40,
-      variance: 0.01,
+      min: 5,
+      max: 30,
+      variance: 2.5,
       weekendReduction: true,
     }),
   };
 };
 
-export const dataLoaderActions16Core = async (
+export const dataLoaderPrMerge = async (
   _isDemo: boolean,
 ): Promise<GraphData> => {
   return {
-    type: "Actions16Core",
+    type: "PrMerge",
     version: "0.1",
     data: generateDailyData({
       startDate: new Date(
         Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
       ),
       endDate: new Date(),
-      min: 30,
-      max: 80,
-      variance: 0.1,
+      min: 5,
+      max: 30,
+      variance: 2.5,
       weekendReduction: true,
     }),
   };
