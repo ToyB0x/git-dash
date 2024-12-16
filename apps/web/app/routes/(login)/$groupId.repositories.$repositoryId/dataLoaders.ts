294648afbebd74cf143e9aa9cf7ff89268b18728
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-17 08:17:15 +0900

    add 4key loader

--- apps/web/app/routes/(login)/$groupId.repositories.$repositoryId/dataLoaders.ts
+++ apps/web/app/routes/(login)/$groupId.repositories.$repositoryId/dataLoaders.ts
@@ -2,7 +2,14 @@ import { generateDailyData } from "@/lib/generateDailyData";
 
 type GraphData = {
   version: "0.1";
-  type: "PrOpen" | "PrMerge" | "Reviews";
+  type:
+    | "PrOpen"
+    | "PrMerge"
+    | "Reviews"
+    | "Release"
+    | "ChangeLeadTime"
+    | "ChangeFailureRate"
+    | "FailedDeploymentRecoveryTime";
   data: {
     date: Date;
     value: number;
@@ -61,6 +68,82 @@ export const dataLoaderReviews = async (
       min: 1,
       max: 15,
       variance: 1,
+      weekendReduction: true,
+    }),
+  };
+};
+
+export const dataLoaderRelease = async (
+  _isDemo: boolean,
+): Promise<GraphData> => {
+  return {
+    type: "Release",
+    version: "0.1",
+    data: generateDailyData({
+      startDate: new Date(
+        Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
+      ),
+      endDate: new Date(),
+      min: 1,
+      max: 6,
+      variance: 2.5,
+      weekendReduction: true,
+    }),
+  };
+};
+
+export const dataLoaderChangeLeadTime = async (
+  _isDemo: boolean,
+): Promise<GraphData> => {
+  return {
+    type: "ChangeLeadTime",
+    version: "0.1",
+    data: generateDailyData({
+      startDate: new Date(
+        Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
+      ),
+      endDate: new Date(),
+      min: 10,
+      max: 50,
+      variance: 0.01,
+      weekendReduction: true,
+    }),
+  };
+};
+
+export const dataLoaderChangeFailureRate = async (
+  _isDemo: boolean,
+): Promise<GraphData> => {
+  return {
+    type: "ChangeFailureRate",
+    version: "0.1",
+    data: generateDailyData({
+      startDate: new Date(
+        Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
+      ),
+      endDate: new Date(),
+      min: 1,
+      max: 8,
+      variance: 0.3,
+      weekendReduction: true,
+    }),
+  };
+};
+
+export const dataLoaderFailedDeploymentRecoveryTime = async (
+  _isDemo: boolean,
+): Promise<GraphData> => {
+  return {
+    type: "FailedDeploymentRecoveryTime",
+    version: "0.1",
+    data: generateDailyData({
+      startDate: new Date(
+        Date.now() - 800 /* 2years ago */ * 24 * 60 * 60 * 1000,
+      ),
+      endDate: new Date(),
+      min: 1,
+      max: 6,
+      variance: 2.5,
       weekendReduction: true,
     }),
   };
