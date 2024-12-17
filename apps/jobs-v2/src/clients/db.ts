2b5c3d1fe1ee9fa8c1f4be534fab044fabb77153
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-17 16:44:48 +0900

    feat: add aggregate Organization

--- apps/jobs-v2/src/clients/db.ts
+++ apps/jobs-v2/src/clients/db.ts
@@ -1,5 +1,3 @@
 import { PrismaClient } from "@repo/db-job";
 
-export const dbClient = () => {
-  return new PrismaClient();
-};
+export const dbClient = new PrismaClient();
--- apps/jobs-v2/src/clients/github.ts
+++ apps/jobs-v2/src/clients/github.ts
@@ -1,12 +1,9 @@
 import { env } from "@/env";
 import { GraphQLClient } from "graphql-request";
 
-export const githubClient = new GraphQLClient(
-  "https://api.github.com/graphql",
-  {
-    headers: {
-      Authorization: `Bearer ${env.GDASH_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      "X-Github-Next-Global-ID": "1",
-    },
+export const ghClient = new GraphQLClient("https://api.github.com/graphql", {
+  headers: {
+    Authorization: `Bearer ${env.GDASH_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    "X-Github-Next-Global-ID": "1",
   },
-);
+});
