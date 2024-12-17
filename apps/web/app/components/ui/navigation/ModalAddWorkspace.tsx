70fbbf2b63d60c83fade6b7ad573310b4a27bd6e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-17 14:28:04 +0900

    feat: update workspace modal

--- apps/web/app/components/ui/navigation/ModalAddWorkspace.tsx
+++ apps/web/app/components/ui/navigation/ModalAddWorkspace.tsx
@@ -1,4 +1,3 @@
-import { Badge } from "@/components/Badge";
 import { Button } from "@/components/Button";
 import {
   Dialog,
@@ -13,14 +12,6 @@ import {
 import { DropdownMenuItem } from "@/components/Dropdown";
 import { Input } from "@/components/Input";
 import { Label } from "@/components/Label";
-import { RadioCardGroup, RadioCardItem } from "@/components/RadioCard";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/Select";
 
 export const databases: {
   label: string;
@@ -80,99 +71,16 @@ export function ModalAddWorkspace({
               <DialogDescription className="mt-1 text-sm leading-6">
                 With free plan, you can add up to 10 workspaces.
               </DialogDescription>
-              <div className="mt-4 grid grid-cols-2 gap-4">
-                <div>
-                  <Label htmlFor="workspace-name" className="font-medium">
-                    Workspace name
-                  </Label>
-                  <Input
-                    id="workspace-name"
-                    name="workspace-name"
-                    placeholder="my_workspace"
-                    className="mt-2"
-                  />
-                </div>
-                <div>
-                  <Label htmlFor="starter-kit" className="font-medium">
-                    Starter kit
-                  </Label>
-                  <Select defaultValue="empty-workspace">
-                    <SelectTrigger
-                      id="starter-kit"
-                      name="starter-kit"
-                      className="mt-2"
-                    >
-                      <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="empty-workspace">
-                        None - Empty workspace
-                      </SelectItem>
-                      <SelectItem value="commerce-analytics">
-                        Commerce analytics
-                      </SelectItem>
-                      <SelectItem value="product-analytics">
-                        Product analytics
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="col-span-full">
-                  <Label htmlFor="database-region" className="font-medium">
-                    Database region
-                  </Label>
-                  <Select defaultValue="europe-west-01">
-                    <SelectTrigger
-                      id="database-region"
-                      name="database-region"
-                      className="mt-2"
-                    >
-                      <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="europe-west-01">
-                        europe-west-01
-                      </SelectItem>
-                      <SelectItem value="us-east-02">us-east-02</SelectItem>
-                      <SelectItem value="us-west-01">us-west-01</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <p className="mt-2 text-xs text-gray-500">
-                    For best performance, choose a region closest to your
-                    application.
-                  </p>
-                </div>
-              </div>
-              <div className="mt-4">
-                <Label htmlFor="database" className="font-medium">
-                  Database configuration
+              <div className="mt-6">
+                <Label htmlFor="workspace-name" className="font-medium">
+                  Workspace name
                 </Label>
-                <RadioCardGroup
-                  defaultValue={databases[0]?.value}
-                  className="mt-2 grid grid-cols-1 gap-4 text-sm md:grid-cols-2"
-                >
-                  {databases.map((database) => (
-                    <RadioCardItem key={database.value} value={database.value}>
-                      <div className="flex items-start gap-3">
-                        <div>
-                          {database.isRecommended ? (
-                            <div className="flex items-center gap-2">
-                              <span className="leading-5">
-                                {database.label}
-                              </span>
-                              <Badge>Recommended</Badge>
-                            </div>
-                          ) : (
-                            <span>{database.label}</span>
-                          )}
-                          <p className="mt-1 text-xs text-gray-500">
-                            1/8 vCPU, 1 GB RAM
-                          </p>
-                        </div>
-                      </div>
-                    </RadioCardItem>
-                  ))}
-                </RadioCardGroup>
+                <Input
+                  id="workspace-name"
+                  name="workspace-name"
+                  placeholder="my_workspace"
+                  className="mt-2"
+                />
               </div>
             </DialogHeader>
             <DialogFooter className="mt-6">
