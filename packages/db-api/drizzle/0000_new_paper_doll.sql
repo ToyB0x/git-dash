1b4513386868ead9633426fe7ae31eb08ca5d39e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-19 08:00:58 +0900

    generate sql

--- packages/db-api/drizzle/0000_new_paper_doll.sql
+++ packages/db-api/drizzle/0000_new_paper_doll.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `group` (
+CREATE TABLE `workspace` (
 	`id` text(8) PRIMARY KEY NOT NULL,
 	`displayName` text(24) NOT NULL,
+	`api_token` text(32) NOT NULL,
 	`createdAt` integer NOT NULL,
 	`updatedAt` integer NOT NULL
 );
@@ -10,11 +11,11 @@ CREATE TABLE `report` (
 	`status` text NOT NULL,
 	`createdAt` integer NOT NULL,
 	`updatedAt` integer NOT NULL,
-	`group_id` text NOT NULL,
-	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`) ON UPDATE cascade ON DELETE cascade
+	`workspace_id` text NOT NULL,
+	FOREIGN KEY (`workspace_id`) REFERENCES `workspace`(`id`) ON UPDATE cascade ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE INDEX `idx_report_group_id` ON `report` (`group_id`);--> statement-breakpoint
+CREATE INDEX `idx_report_workspace_id` ON `report` (`workspace_id`);--> statement-breakpoint
 CREATE TABLE `user` (
 	`id` text(8) PRIMARY KEY NOT NULL,
 	`email` text(256) NOT NULL,
@@ -25,11 +26,11 @@ CREATE TABLE `user` (
 --> statement-breakpoint
 CREATE UNIQUE INDEX `uq_user_firebase_uid` ON `user` (`firebase_uid`);--> statement-breakpoint
 CREATE UNIQUE INDEX `uq_user_lower_email` ON `user` (lower("email"));--> statement-breakpoint
-CREATE TABLE `users_to_groups` (
+CREATE TABLE `users_to_workspaces` (
 	`user_id` text NOT NULL,
-	`group_id` text NOT NULL,
+	`workspace_id` text NOT NULL,
 	`role` text NOT NULL,
-	PRIMARY KEY(`user_id`, `group_id`),
+	PRIMARY KEY(`user_id`, `workspace_id`),
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE cascade ON DELETE cascade,
-	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`) ON UPDATE cascade ON DELETE cascade
+	FOREIGN KEY (`workspace_id`) REFERENCES `workspace`(`id`) ON UPDATE cascade ON DELETE cascade
 );
--- packages/db-api/drizzle/0001_funny_earthquake.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `group` ADD `api_token` text(32) NOT NULL;
