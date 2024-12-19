1d4ec63b2be61783c766ec5b8d0af8f2898b91a9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-19 15:40:48 +0900

    test: add E2E for demo pages

---
+++ E2E/internal/test/demo-pages.spec.ts
@@ -0,0 +1,40 @@
+import { test } from "@playwright/test";
+
+test("can sign up and redirect", async ({ page }) => {
+  // overview page
+  await page.goto("http://localhost:10000/demo");
+  await page.getByText("128").click();
+
+  // cost page
+  await page.getByRole("link", { name: "Cost", exact: true }).click();
+  await page.getByText("96.1%").click();
+  await page.getByText("$3293.5").click();
+
+  // prs page
+  await page.getByRole("link", { name: "PRs" }).click();
+  await page.getByText("9.1 days").click();
+
+  // reviews page
+  await page.getByRole("link", { name: "Reviews" }).click();
+  await page.getByText("3.1 hours").click();
+
+  // releases page
+  await page.getByRole("link", { name: "Releases" }).click();
+  await page.getByText("9.1 days").click();
+
+  // four keys page
+  await page.getByRole("link", { name: "Four Keys" }).click();
+  await page.getByRole("link", { name: "org/api" }).click();
+
+  // vulns page
+  await page.getByRole("link", { name: "Vulns" }).click();
+  await page.getByText("141").click();
+
+  // users page
+  await page.getByRole("link", { name: "Users" }).click();
+  await page.getByRole("link", { name: "C0d3r" }).click();
+
+  // repositories page
+  await page.getByRole("link", { name: "Repositories" }).click();
+  await page.getByRole("link", { name: "org/api" }).click();
+});
