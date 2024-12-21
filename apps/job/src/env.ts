fa3dbebe60dd0596249e0cf63169383826ec6bc3
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-21 09:23:52 +0900

    update env

--- apps/job/src/env.ts
+++ apps/job/src/env.ts
@@ -8,10 +8,12 @@ const envSchema = object({
     literal("gdash-stg"),
     literal("gdash-prd"),
   ]),
+  GDASH_GITHUB_INTERNAL_APP_ID: string(),
+  GDASH_GITHUB_INTERNAL_APP_PRIVATE_KEY_STRING: string(),
   GDASH_WORKSPACE_ID: string(),
   GDASH_WORKSPACE_API_KEY: string(),
   GDASH_GITHUB_ORGANIZATION_NAME: string(),
-  GDASH_GITHUB_PERSONAL_ACCESS_TOKEN: string(),
+  // GDASH_GITHUB_PERSONAL_ACCESS_TOKEN: string(),
 });
 
 export const env = parse(envSchema, process.env);
