517227d2261a3836126c4f6b182a572c959cf511
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-23 03:19:06 +0900

    feat: add costs schema (#310)

---
+++ packages/schema/src/$workspaceId/costs.ts
@@ -0,0 +1,32 @@
+import * as v from "valibot";
+
+const version = "0.1" as const;
+const typeName = "$workspaceId.costs" as const;
+
+const schema = v.object({
+  reportId: v.string(),
+  type: v.literal(typeName),
+  version: v.literal(version), // latest version
+  stats: v.array(
+    v.object({
+      date: v.string(),
+      cost: v.number(),
+    }),
+  ),
+});
+
+const fixture = {
+  reportId: "1",
+  type: typeName,
+  version: version,
+  stats: [{ date: new Date().toDateString(), cost: 11.2 }],
+} satisfies Schema;
+
+export const stat = {
+  type: typeName,
+  version: version,
+  schema,
+  fixture,
+};
+
+export type Schema = v.InferInput<typeof schema>;
