4dcb2e53b5cba56950f71189d1fd0abeef24f458
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-25 01:52:05 +0900

    fix

--- packages/db-shared/src/user.ts
+++ packages/db-shared/src/user.ts
@@ -3,6 +3,6 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const userTbl = sqliteTable("user", {
   id: int().primaryKey(), // github user id
   login: text("title").notNull(),
-  avatarUrl: int("avatar_url").notNull(),
+  avatarUrl: text("avatar_url").notNull(),
   updatedAt: int({ mode: "timestamp_ms" }).notNull(),
 });
