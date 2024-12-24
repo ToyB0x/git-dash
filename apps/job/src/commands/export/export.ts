ebff404700c9ec7ea8bf3f3e3b6d5b6989894e6b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-24 10:09:15 +0900

    export cost

--- apps/job/src/commands/export/export.ts
+++ apps/job/src/commands/export/export.ts
@@ -4,10 +4,7 @@ import {
   type Schema as SchemaActionUsageCurrentCycle,
   stat as statActionUsageCurrentCycle,
 } from "@repo/schema/statActionUsageCurrentCycle";
-import {
-  type Schema as SchemaCost,
-  stat as statCost,
-} from "@repo/schema/statCost";
+import { cost } from "./cost";
 import { prepare } from "./prepare";
 import { repositories } from "./repositories";
 
@@ -24,38 +21,10 @@ export const exportByOrganization = async (
     callback: repositories(reportId),
   });
 
-  // export cost
-  const usages = await dbClient.workflowUsageRepoDaily.findMany({
-    where: { scanId },
+  await step({
+    stepName: "export:cost",
+    callback: cost({ scanId, reportId }),
   });
-
-  const cost = usages.reduce((acc, usage) => {
-    // https://docs.github.com/ja/billing/managing-billing-for-your-products/managing-billing-for-github-actions/about-billing-for-github-actions#per-minute-rates-for-standard-runners
-    switch (usage.runner) {
-      case "UBUNTU":
-        return acc + Math.ceil(usage.totalMs / (1000 * 60)) * 0.008;
-      case "UBUNTU_4_CORE":
-        return acc + Math.ceil(usage.totalMs / (1000 * 60)) * 0.016;
-      case "UBUNTU_16_CORE":
-        return acc + Math.ceil(usage.totalMs / (1000 * 60)) * 0.064;
-      case "MACOS":
-        return acc + Math.ceil(usage.totalMs / (1000 * 60)) * 0.08;
-      default:
-        throw Error("Please Add Runner Type");
-    }
-  }, 0);
-
-  const costJson = {
-    reportId,
-    type: statCost.type,
-    version: statCost.version,
-    stats: {
-      date: new Date(Date.now() - 24 * 60 * 60 * 1000).toDateString(),
-      cost: Math.round(cost * 10) / 10, // 0.1 $ precision
-    },
-  } satisfies SchemaCost;
-
-  await hc["public-api"].reports.$post({ json: costJson });
 
   // export cost
   const actionUsageCurrentCycle =
