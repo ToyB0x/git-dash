d72d917126c206658f42bf21531239d8d5aadc4f
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-24 16:33:07 +0900

    generate schema

---
+++ packages/db-shared/drizzle/0000_gigantic_corsair.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usage_current_cycle_action_org` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`cost` integer NOT NULL,
+	`runner_type` text,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL
+);
