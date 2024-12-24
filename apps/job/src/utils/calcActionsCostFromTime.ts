c91e1f1cf5fadd269fdb7ea48d70d7d7b494a64c
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-24 18:35:08 +0900

    fix

--- apps/job/src/utils/calcActionsCostFromTime.ts
+++ apps/job/src/utils/calcActionsCostFromTime.ts
@@ -2,77 +2,78 @@ export const calcActionsCostFromTime = ({
   runner,
   milliSec,
 }: { runner: string; milliSec: number }) => {
+  const minutes = Math.ceil(milliSec / (1000 * 60));
   // https://docs.github.com/ja/billing/managing-billing-for-your-products/managing-billing-for-github-actions/about-billing-for-github-actions#per-minute-rates-for-standard-runners
   switch (runner) {
     case "UBUNTU":
       return {
         runner,
-        cost: milliSec * 0.008,
+        cost: minutes * 0.008,
       };
     case "MACOS":
       return {
         runner,
-        cost: milliSec * 0.08,
+        cost: minutes * 0.08,
       };
     case "WINDOWS":
       return {
         runner,
-        cost: milliSec * 0.016,
+        cost: minutes * 0.016,
       };
     case "ubuntu_4_core":
       return {
         runner,
-        cost: milliSec * 0.016,
+        cost: minutes * 0.016,
       };
     case "ubuntu_8_core":
       return {
         runner,
-        cost: milliSec * 0.032,
+        cost: minutes * 0.032,
       };
     case "ubuntu_16_core":
       return {
         runner,
-        cost: milliSec * 0.064,
+        cost: minutes * 0.064,
       };
     case "ubuntu_32_core":
       return {
         runner,
-        cost: milliSec * 0.128,
+        cost: minutes * 0.128,
       };
     case "ubuntu_64_core":
       return {
         runner,
-        cost: milliSec * 0.256,
+        cost: minutes * 0.256,
       };
     case "windows_4_core":
       return {
         runner,
-        cost: milliSec * 0.032,
+        cost: minutes * 0.032,
       };
     case "windows_8_core":
       return {
         runner,
-        cost: milliSec * 0.064,
+        cost: minutes * 0.064,
       };
     case "windows_16_core":
       return {
         runner,
-        cost: milliSec * 0.128,
+        cost: minutes * 0.128,
       };
     case "windows_32_core":
       return {
         runner,
-        cost: milliSec * 0.256,
+        cost: minutes * 0.256,
       };
     case "windows_64_core":
       return {
         runner,
-        cost: milliSec * 0.512,
+        cost: minutes * 0.512,
       };
     case "macos_12_core":
       return {
         runner,
-        cost: milliSec * 0.12,
+        cost: minutes * 0.12,
       };
     case "total":
       return null; // ignore total
