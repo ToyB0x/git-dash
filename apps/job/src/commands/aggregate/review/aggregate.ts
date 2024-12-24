015a10cbdeb50b9c23216a8b4390b024d5bde0ec
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-25 03:39:47 +0900

    fix aggregation

--- apps/job/src/commands/aggregate/review/aggregate.ts
+++ apps/job/src/commands/aggregate/review/aggregate.ts
@@ -1,8 +1,9 @@
 import { getOctokit, sharedDbClient } from "@/clients";
 import { env } from "@/env";
 import { logger } from "@/utils";
-import { reviewTbl } from "@repo/db-shared";
+import { prTbl, reviewTbl } from "@repo/db-shared";
 import { PromisePool } from "@supercharge/promise-pool";
+import { eq } from "drizzle-orm";
 
 export const aggregate = async (
   repositories: { id: number; name: string }[],
@@ -13,7 +14,11 @@ export const aggregate = async (
     // 8 concurrent requests
     // ref: https://docs.github.com/ja/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28#about-secondary-rate-limits
     .withConcurrency(8)
-    .process(async (repository) => {
+    .process(async (repository, i) => {
+      logger.info(
+        `Start aggregate:review-cost ${repository.name} (${i + 1}/${repositories.length})`,
+      );
+
       // ref: https://docs.github.com/ja/rest/pulls/comments?apiVersion=2022-11-28#list-review-comments-in-a-repository
       const reviews = await octokit.paginate(
         octokit.rest.pulls.listReviewCommentsForRepo,
@@ -44,6 +49,16 @@ export const aggregate = async (
         // ref: https://docs.github.com/ja/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28#about-secondary-rate-limits
         .withConcurrency(1)
         .process(async (review) => {
+          if (review.pull_request_review_id === null) return;
+
+          const prs = await sharedDbClient
+            .select()
+            .from(prTbl)
+            .where(eq(prTbl.id, review.pull_request_review_id));
+
+          // Skip self review
+          if (prs.find((pr) => pr.authorId === review.user.id)) return;
+
           await sharedDbClient
             .insert(reviewTbl)
             .values({
