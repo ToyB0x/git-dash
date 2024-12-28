b3617ec3c9811fa48507ff56dd09befb1e2943aa
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-28 17:43:58 +0900

    chore: add error log (#379)

--- apps/job/src/commands/aggregate/release/aggregate.ts
+++ apps/job/src/commands/aggregate/release/aggregate.ts
@@ -1,5 +1,6 @@
 import { getOctokit, sharedDbClient } from "@/clients";
 import { env } from "@/env";
+import { logger } from "@/utils";
 import { releaseTbl } from "@repo/db-shared";
 import { PromisePool } from "@supercharge/promise-pool";
 import { desc, notInArray } from "drizzle-orm";
@@ -8,7 +9,7 @@ export const aggregate = async (
   repositories: { id: number; name: string }[],
 ) => {
   const octokit = await getOctokit();
-  await PromisePool.for(repositories)
+  const { errors } = await PromisePool.for(repositories)
     // parent: 8 , child: 10 = max 80 concurrent requests
     // ref: https://docs.github.com/ja/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28#about-secondary-rate-limits
     .withConcurrency(8)
@@ -78,6 +79,10 @@ export const aggregate = async (
           });
       }
     });
+
+  if (errors.length > 0) {
+    logger.error(JSON.stringify(errors));
+  }
 
   const latestReleases = await sharedDbClient
     .select()
