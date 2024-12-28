cc02d077b31a4e864edddbf8cbbbde8cef908e26
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-28 17:49:44 +0900

    fix: handle release authors

--- packages/db-shared/src/release.ts
+++ packages/db-shared/src/release.ts
@@ -1,16 +1,15 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { repositoryTbl } from "./repository";
-import { userTbl } from "./user";
 
 export const releaseTbl = sqliteTable("release", {
   id: int().primaryKey(), // github release id
   url: text("url").notNull(),
-  authorId: int("author_id")
-    .notNull()
-    .references(() => userTbl.id, {
-      onUpdate: "no action",
-      onDelete: "no action",
-    }),
+  authorId: int("author_id").notNull(),
+  // NOTE: Release 収集時点ではユーザー情報は取得できていないため、Relationを外す
+  // .references(() => userTbl.id, {
+  //   onUpdate: "no action",
+  //   onDelete: "no action",
+  // }),
   title: text("name"),
   body: text("body"),
   createdAt: int({ mode: "timestamp_ms" }).notNull(),
