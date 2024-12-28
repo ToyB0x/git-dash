b9384b916ef40349e838a5b835e41a18a6e17700
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-28 10:09:59 +0900

    refactor: export job

--- apps/job/src/commands/export/export.ts
+++ apps/job/src/commands/export/export.ts
@@ -1,30 +1,12 @@
 import { logger, step } from "@/utils";
-import { cost } from "./cost";
 import { db } from "./db";
 import { finalize } from "./finalize";
 import { prepare } from "./prepare";
-import { repositories } from "./repositories";
-import { usage } from "./usage";
 
 export const exportByWorkspace = async (): Promise<void> => {
-  const { scanId, reportId } = await step({
+  const { reportId } = await step({
     stepName: "export:prepare",
     callback: prepare(),
-  });
-
-  await step({
-    stepName: "export:repositories",
-    callback: repositories(reportId),
-  });
-
-  await step({
-    stepName: "export:cost",
-    callback: cost({ scanId, reportId }),
-  });
-
-  await step({
-    stepName: "export:usage",
-    callback: usage({ scanId, reportId }),
   });
 
   await step({
--- apps/job/src/commands/export/prepare/prepare.ts
+++ apps/job/src/commands/export/prepare/prepare.ts
@@ -1,14 +1,8 @@
-import { dbClient, hc } from "@/clients";
+import { hc } from "@/clients";
 import { env } from "@/env";
 import { logger } from "@/utils";
 
 export const prepare = async () => {
-  const { id: scanId } = await dbClient.scan.findFirstOrThrow({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
   const res = await hc["public-api"]["reports-meta"][":workspaceId"].$post({
     param: { workspaceId: env.GDASH_WORKSPACE_ID },
   });
@@ -19,5 +13,5 @@ export const prepare = async () => {
   }
 
   const { id: reportId } = await res.json();
-  return { scanId, reportId };
+  return { reportId };
 };
