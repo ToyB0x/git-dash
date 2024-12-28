435431f47fdc38598d00092f4387677fc758cd43
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-29 05:34:05 +0900

    rename db file

--- apps/job/src/clients/db.ts
+++ apps/job/src/clients/db.ts
@@ -1,6 +1,7 @@
+import { env } from "@/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-export const filePath = "../../packages/db-shared/sqlite/shared.db";
+export const filePath = `../../packages/db-shared/sqlite/${env.GDASH_WORKSPACE_ID}.db`;
 const sqliteClient = createClient({ url: `file:${filePath}` });
 export const sharedDbClient = drizzle({ client: sqliteClient });
