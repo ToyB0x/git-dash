27f5846d6caa27a4cb7b605dfbf07fa5e346ea5c
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-28 11:42:36 +0900

    fix type

--- apps/api/src/routes/public-api/reports/post.ts
+++ apps/api/src/routes/public-api/reports/post.ts
@@ -2,10 +2,7 @@ import { vValidator } from "@hono/valibot-validator";
 import { getR2Path } from "@repo/schema/path";
 import { stat as statActionUsageCurrentCycle } from "@repo/schema/statActionUsageCurrentCycle";
 import { stat as statCost } from "@repo/schema/statCost";
-import {
-  type Schema as SchemaCosts,
-  stat as statCosts,
-} from "@repo/schema/statCosts";
+import { stat as statCosts } from "@repo/schema/statCosts";
 import { stat as statMerged } from "@repo/schema/statMerged";
 import { stat as statRepositories } from "@repo/schema/statRepositories";
 import { stat as statReviews } from "@repo/schema/statReviews";
@@ -39,52 +36,10 @@ const handlers = factory.createHandlers(
   validator,
   async (c) => {
     const validated = c.req.valid("json");
-
-    // handle costs via daily cost report
-    if (validated.type === statCost.type) {
-      const prevReportR2 = await c.env.REPORT_BUCKET.get(
-        getR2Path({
-          workspaceId: c.var.validWorkspaceId,
-          reportId: validated.reportId,
-          type: statCosts.type,
-        }),
-      );
-
-      let prevReportParsed: SchemaCosts | undefined = undefined;
-      if (prevReportR2) {
-        prevReportParsed = v.parse(statCosts.schema, await prevReportR2.json());
-      }
-
-      // TODO: 同日中に2回レポート送信された場合にレコードが重複するバグがあるため調整(year / month / day を比較する)
-      const merged = prevReportParsed
-        ? {
-            ...prevReportParsed,
-            stats: [...prevReportParsed.stats, validated.stats],
-          }
-        : {
-            reportId: validated.reportId,
-            type: statCosts.type,
-            version: statCosts.version,
-            stats: [validated.stats],
-          };
-
-      await c.env.REPORT_BUCKET.put(
-        getR2Path({
-          workspaceId: c.var.validWorkspaceId,
-          reportId: validated.reportId,
-          type: statCosts.type,
-        }),
-        JSON.stringify(merged),
-      );
-
-      return c.json({ success: true });
-    }
-
     await c.env.REPORT_BUCKET.put(
       getR2Path({
         workspaceId: c.var.validWorkspaceId,
         reportId: validated.reportId,
-        type: validated.type,
       }),
       JSON.stringify(validated),
     );
