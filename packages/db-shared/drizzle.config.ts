e4edd297ff99493cd54e12a405563dbb82e94f0a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-29 07:37:59 +0900

    fix: db migration

--- packages/db-shared/drizzle.config.ts
+++ packages/db-shared/drizzle.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+// biome-ignore lint/complexity/useLiteralKeys: <explanation>
+const DB_FILE_NAME = process.env["DB_FILE_NAME"];
+if (!DB_FILE_NAME) {
+  throw new Error("DB_FILE_NAME is required");
+}
+
 export default defineConfig({
   schema: "./src/index.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: "./sqlite/shared.db",
+    url: DB_FILE_NAME,
   },
 });
