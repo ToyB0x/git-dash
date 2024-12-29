8202380bdc03b44ea44502776a2bcbbab7d6b621
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-29 22:47:54 +0900

    fix: db migration

--- apps/job/src/commands/db/migrate.ts
+++ apps/job/src/commands/db/migrate.ts
@@ -1,6 +1,5 @@
 import { execSync } from "node:child_process";
 import fs from "node:fs";
-import { isBuffer } from "node:util";
 import { filePath } from "@/clients";
 import { logger } from "@/utils";
 
@@ -13,13 +12,19 @@ export const migrate = () => {
     logger.info(stdout.toString());
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   } catch (e: any) {
-    if (isBuffer(e.stdout)) logger.info(e.stdout.toString());
-    if (isBuffer(e.stderr)) logger.error(e.stderr.toString());
+    if (Buffer.isBuffer(e.stdout)) logger.info(e.stdout.toString());
+    if (Buffer.isBuffer(e.stderr)) logger.error(e.stderr.toString());
 
     logger.error(
       "Failed to migrate database file, destroying(rename) the broken database file",
     );
 
     fs.renameSync(filePath, `${filePath}-bk-${Date.now()}`);
+
+    logger.info("Createing new database file");
+    const stdout = execSync(
+      `DB_FILE_NAME=${filePath} pnpm --filter @repo/db-shared db:migrate`,
+    );
+    logger.info(stdout.toString());
   }
 };
