3189d1be18e5ffdb2425953df0cae55df0980297
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-29 21:59:35 +0900

    update table

--- packages/db-shared/src/workflowUsageCurrentCycleOrg.ts
+++ packages/db-shared/src/workflowUsageCurrentCycleOrg.ts
@@ -8,11 +8,12 @@ export const workflowUsageCurrentCycleOrgTbl = sqliteTable(
     day: int().notNull(),
     dollar: int().notNull(),
     // TODO: DB容量を削減したい場合はrunnerTypeを別のマスタテーブルに切り出すことを検討
+    // (eg: 14 bytes(ubuntu_16_core text size) * 360 days * 10 runnerType = 50.4kb)
     runnerType: text("runner_type").notNull(),
     createdAt: int({ mode: "timestamp_ms" }).notNull(),
     updatedAt: int({ mode: "timestamp_ms" }).notNull(),
   },
   (t) => ({
-    pk: primaryKey({ columns: [t.year, t.month, t.day] }),
+    pk: primaryKey({ columns: [t.year, t.month, t.day, t.runnerType] }),
   }),
 );
