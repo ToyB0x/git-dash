683afc4f6b57696d0b55b90fc8a64c11423b4f5b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-29 23:24:12 +0900

    perf: run vacuum for resize db (#392)

--- apps/job/src/commands/export/db/db.ts
+++ apps/job/src/commands/export/db/db.ts
@@ -1,12 +1,16 @@
 import { readFile } from "node:fs/promises";
-import { filePath, hc } from "@/clients";
+import { filePath, hc, sharedDbClient } from "@/clients";
 import { stat } from "@repo/schema/statFile";
+import { sql } from "drizzle-orm";
 
 export const db = async ({
   reportId,
 }: {
   reportId: string;
 }) => {
+  // VACUUM is used to rebuild the database file, it is used to optimize the database file
+  await sharedDbClient.run(sql`VACUUM;`);
+
   const file = await readFile(filePath);
   const gziped = await gzip(file);
 
