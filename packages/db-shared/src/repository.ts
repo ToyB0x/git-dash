77929c1df07631b20c79a6c91cf7862e10f5bde4
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-30 11:59:14 +0900

    feat: add alert table

--- packages/db-shared/src/repository.ts
+++ packages/db-shared/src/repository.ts
@@ -4,6 +4,10 @@ export const repositoryTbl = sqliteTable("repository", {
   id: int().primaryKey(), // github repository id
   name: text("name").notNull(), // repo name (ex. drizzle-core section of "@drizzle-org/drizzle-core")
   owner: text("owner").notNull(), // owner login name
+  enabledAlert: int("enabled_alert", { mode: "boolean" }),
+  enabledAlertCheckedAt: int("enabled_alert_checked_at", {
+    mode: "timestamp_ms",
+  }),
   createdAt: int({ mode: "timestamp_ms" }).notNull(),
   updatedAt: int({ mode: "timestamp_ms" }).notNull(),
 });
