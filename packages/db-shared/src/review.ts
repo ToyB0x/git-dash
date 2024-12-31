caa4d28c3598438cb166ea0203bfa37c347425c2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-31 09:27:46 +0900

    add relation

--- packages/db-shared/src/review.ts
+++ packages/db-shared/src/review.ts
@@ -1,8 +1,14 @@
 import { int, sqliteTable } from "drizzle-orm/sqlite-core";
+import { prTbl } from "./pr";
 
 export const reviewTbl = sqliteTable("review", {
   id: int().primaryKey(), // github review id
   createdAt: int({ mode: "timestamp_ms" }).notNull(),
-  reviewerId: int("reviewer_id").notNull(),
-  prId: int("pr_id").notNull(),
+  reviewerId: int("reviewer_id").notNull(), // 集計順序の都合でUserTblとのリレーションは持たない
+  prId: int("pr_id")
+    .notNull()
+    .references(() => prTbl.id, {
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    }),
 });
--- packages/db-shared/src/timeline.ts
+++ packages/db-shared/src/timeline.ts
@@ -1,5 +1,5 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
+import { prTbl } from "./pr";
 
 export const eventTypes = [
   "closed",
@@ -10,12 +10,17 @@ export const eventTypes = [
   "review_requested",
   "review_request_removed",
   "reviewed",
-] as const
+] as const;
 
 export const timelineTbl = sqliteTable("timeline", {
   id: int().primaryKey(), // github timeline id
-  prId: int("pr_id").notNull(),
-  actorId: int("actor_id").notNull(),
+  prId: int("pr_id")
+    .notNull()
+    .references(() => prTbl.id, {
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    }),
+  actorId: int("actor_id").notNull(), // 集計順序の都合でUserTblとのリレーションは持たない
   eventType: text("event_type", {
     // ref: https://docs.github.com/ja/rest/using-the-rest-api/issue-event-types?apiVersion=2022-11-28
     enum: eventTypes,
