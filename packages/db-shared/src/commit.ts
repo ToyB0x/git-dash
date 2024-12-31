b353d88f67befa94ac8733385c5f5eeb646cb7ea
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-12-31 10:19:22 +0900

    fix column

--- packages/db-shared/src/commit.ts
+++ packages/db-shared/src/commit.ts
@@ -5,6 +5,6 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const commitTbl = sqliteTable("commit", {
   id: text().primaryKey(), // github commit hash id
   repositoryId: int("repository_id").notNull(), // 集計順序を柔軟に変更できるようリレーションは持たない(一定期間経過後、それぞれ独立してレコード削除するので問題ない)
-  authorName: int("author_name").notNull(), // 集計順序の都合でUserTblとのリレーションは持たない
+  authorName: text("author_name").notNull(), // 集計順序の都合でUserTblとのリレーションは持たない
   createdAt: int({ mode: "timestamp_ms" }).notNull(), // commit data
 });
