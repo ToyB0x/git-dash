835ee97ebcceaf29077d7e0560276f573e9fe9c2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-01 09:31:10 +0900

    drop tables

---
+++ packages/db-shared/drizzle/0014_bumpy_namorita.sql
@@ -0,0 +1,4 @@
+DROP TABLE `pr_commit`;--> statement-breakpoint
+DROP TABLE `review`;--> statement-breakpoint
+DROP TABLE `timeline`;--> statement-breakpoint
+DROP TABLE `workflow_usage_current_cycle`;
