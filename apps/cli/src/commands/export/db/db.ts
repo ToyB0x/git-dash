e49f7e007b3537647a2ce3d593540c19559a810b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-03 07:00:45 +0900

    refactor

--- apps/cli/src/commands/export/db/db.ts
+++ apps/cli/src/commands/export/db/db.ts
@@ -1,6 +1,7 @@
 import { readFile } from "node:fs/promises";
 import { getDbClient, getDbPath, type getHonoClient } from "@/clients";
 import type { Configs } from "@/env";
+import { logger } from "@/utils";
 import { sql } from "drizzle-orm";
 
 export const db = async ({
@@ -16,6 +17,14 @@ export const db = async ({
 
   const file = await readFile(getDbPath(configs));
   const gziped = await gzip(file);
+
+  const maxSize = 5 * 1024 * 1024; // 5 MB
+  if (gziped.byteLength > maxSize) {
+    logger.warn(`Database file gzipped size: ${gziped.byteLength} bytes`);
+    throw Error(
+      "Database file gzipped size is too large. max size is 5MB. Please reduce the size of the database file.",
+    );
+  }
 
   await honoClient["public-api"].db.$post({
     form: {
