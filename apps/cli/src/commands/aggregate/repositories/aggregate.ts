7d928fca6a1be60f2e5d7802822c3c44d1f84e7d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-03 04:22:52 +0900

    update aggregate repo

--- apps/cli/src/commands/aggregate/repositories/aggregate.ts
+++ apps/cli/src/commands/aggregate/repositories/aggregate.ts
@@ -3,17 +3,37 @@ import type { Configs } from "@/env";
 import { logger } from "@/utils";
 import { repositoryTbl } from "@git-dash/db";
 import { PromisePool } from "@supercharge/promise-pool";
+import { subDays } from "date-fns";
 
 export const aggregate = async (
   octokit: Awaited<ReturnType<typeof getOctokit>>,
   sharedDbClient: ReturnType<typeof getDbClient>,
   configs: Configs,
+  maxOldReleaseDate?: Date,
 ) => {
   // ref: https://docs.github.com/ja/rest/repos/repos?apiVersion=2022-11-28
-  const repos = await octokit.paginate(octokit.rest.repos.listForOrg, {
-    org: configs.GDASH_GITHUB_ORGANIZATION_NAME,
-    per_page: 100,
-  });
+  const repos = await octokit.paginate(
+    octokit.rest.repos.listForOrg,
+    {
+      org: configs.GDASH_GITHUB_ORGANIZATION_NAME,
+      per_page: 100,
+      sort: "pushed",
+      direction: "desc",
+    },
+    (response, done) => {
+      const maxOld =
+        maxOldReleaseDate ||
+        subDays(new Date(), configs.GDASH_COLLECT_DAYS_HEAVY_TYPE_ITEMS);
+      if (
+        response.data.find(
+          (repo) => !repo.pushed_at || new Date(repo.pushed_at) < maxOld,
+        )
+      ) {
+        done();
+      }
+      return response.data;
+    },
+  );
 
   const { results, errors } = await PromisePool.for(repos)
     // parent: 8 , child: 10 = max 80 concurrent requests
