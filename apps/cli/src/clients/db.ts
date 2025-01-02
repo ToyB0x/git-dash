5c0efad877fd570e9b2b19c0931122cfffef337d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-03 04:06:23 +0900

    update db client

--- apps/cli/src/clients/db.ts
+++ apps/cli/src/clients/db.ts
@@ -1,24 +1,23 @@
 import { execSync } from "node:child_process";
 import type { Configs } from "@/env";
-import { logger } from "@/utils";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-// TODO: CLIとして配布しやすいようにパスを変更する
 export const getDbPath = (configs: Configs) =>
   configs.GDASH_MODE !== "PERSONAL" && configs.GDASH_MODE !== "PERSONAL_SAMPLE"
-    ? `../../packages/db-shared/sqlite/${configs.GDASH_WORKSPACE_ID}.db`
-    : `../../packages/db-shared/sqlite/personal-${configs.GDASH_GITHUB_ORGANIZATION_NAME}.db`;
+    ? `${process.cwd()}/sqlite-${configs.GDASH_WORKSPACE_ID}.db`
+    : `${process.cwd()}/sqlite-personal-${configs.GDASH_GITHUB_ORGANIZATION_NAME}.db`;
 
 export const getDbClient = (configs: Configs) => {
   const filePath = getDbPath(configs);
 
-  if (configs.GDASH_MODE === "PERSONAL") {
-    logger.debug("Migrating database file");
-    const stdout = execSync(
-      `DB_FILE_NAME=${filePath} pnpm --filter @git-dash/db db:migrate`,
-    );
-    logger.debug(stdout.toString());
+  // TODO: 全てのモードでマイグレーションの手順を共通化するようコードをリファクタする(現在は以下モードの時のみ暗黙的にマイグレーションしてしまっている)
+  if (["PERSONAL", "PERSONAL_SAMPLE"].includes(configs.GDASH_MODE)) {
+    const command =
+      configs.GDASH_ENV === "dev"
+        ? "pnpm --filter @git-dash/db db:migrate"
+        : "npx @git-dash/db db:migrate";
+    execSync(`DB_FILE_NAME=${filePath} ${command}`);
   }
 
   const sqliteClient = createClient({ url: `file:${filePath}` });
