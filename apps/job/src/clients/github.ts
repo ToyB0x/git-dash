4879d206b75e5324ab23fd2b1e0b4dd2f996ac3d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-02 12:57:44 +0900

    update github client

--- apps/job/src/clients/github.ts
+++ apps/job/src/clients/github.ts
@@ -1,43 +1,33 @@
-import { env } from "@/env";
+import { execSync } from "node:child_process";
+import type { Configs } from "@/env";
 import { logger } from "@/utils";
-import { GraphQLClient } from "graphql-request";
-import { App } from "octokit";
+import { App, Octokit } from "octokit";
 
-// TODO: in the future, we should use octokit-client directly in the app
-// (waiting for https://github.com/octokit/graphql.js/pull/609)
-export const octokitApp = new App({
-  appId: env.GDASH_GITHUB_INTERNAL_APP_ID,
-  privateKey: env.GDASH_GITHUB_INTERNAL_APP_PRIVATE_KEY_STRING,
-});
-
-export const getGhClient = async () => {
-  const { data: slug } = await octokitApp.octokit.rest.apps.getAuthenticated();
-
-  logger.debug({ slug });
-
-  const { data: getInstallationResult } =
-    await octokitApp.octokit.rest.apps.getOrgInstallation({
-      org: env.GDASH_GITHUB_ORGANIZATION_NAME,
-    });
-
-  logger.debug({ installation_id: getInstallationResult.id });
-
-  const {
-    data: { token },
-  } = await octokitApp.octokit.rest.apps.createInstallationAccessToken({
-    installation_id: getInstallationResult.id,
-  });
-
-  return new GraphQLClient("https://api.github.com/graphql", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "X-Github-Next-Global-ID": "1",
-    },
-  });
+export const getOctokit = async (configs: Configs) => {
+  switch (configs.GDASH_MODE) {
+    case "ORGANIZATION_APP":
+      return await getOctokitApp();
+    case "SINGLE_REPOSITORY":
+      throw Error("Not implemented yet");
+    case "PERSONAL":
+      return await getPersonalOctokit();
+    // exhaustive check
+    default: {
+      const _exhaustiveCheck: never = configs.GDASH_MODE;
+      return _exhaustiveCheck;
+    }
+  }
 };
 
 // NOTE: if you need extend octokit app with installationId
-export const getOctokit = async () => {
+export const getOctokitApp = async (configs: Configs) => {
+  // TODO: in the future, we should use octokit-client directly in the app
+  // (waiting for https://github.com/octokit/graphql.js/pull/609)
+  const octokitApp = new App({
+    appId: configs.GDASH_GITHUB_INTERNAL_APP_ID,
+    privateKey: configs.GDASH_GITHUB_INTERNAL_APP_PRIVATE_KEY_STRING,
+  });
+
   const { data: slug } = await octokitApp.octokit.rest.apps.getAuthenticated();
 
   logger.debug({ slug });
@@ -51,3 +41,48 @@ export const getOctokit = async () => {
 
   return await octokitApp.getInstallationOctokit(getInstallationResult.id);
 };
+
+export const getPersonalOctokit = async () => {
+  try {
+    const stdout = execSync("gh auth token");
+    const oauthToken = stdout.toString().trim();
+
+    return new Octokit({
+      auth: oauthToken,
+    });
+  } catch (e) {
+    console.error(e);
+    console.error("Please install Github CLI and set it up first.");
+    process.exit(1);
+  }
+};
+
+// NOTE: if you need graphql client
+//
+// import { GraphQLClient } from "graphql-request";
+//
+// export const getGhClient = async () => {
+//   const { data: slug } = await octokitApp.octokit.rest.apps.getAuthenticated();
+//
+//   logger.debug({ slug });
+//
+//   const { data: getInstallationResult } =
+//     await octokitApp.octokit.rest.apps.getOrgInstallation({
+//       org: env.GDASH_GITHUB_ORGANIZATION_NAME,
+//     });
+//
+//   logger.debug({ installation_id: getInstallationResult.id });
+//
+//   const {
+//     data: { token },
+//   } = await octokitApp.octokit.rest.apps.createInstallationAccessToken({
+//     installation_id: getInstallationResult.id,
+//   });
+//
+//   return new GraphQLClient("https://api.github.com/graphql", {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       "X-Github-Next-Global-ID": "1",
+//     },
+//   });
+// };
