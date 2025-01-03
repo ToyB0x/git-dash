7ce1eb4f74235fb15d7a17a9c2971493e0d50f05
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-03 12:43:40 +0900

    fix db

--- apps/cli/src/clients/db.ts
+++ apps/cli/src/clients/db.ts
@@ -14,10 +14,15 @@ export const getDbClient = (configs: Configs) => {
   // TODO: 全てのモードでマイグレーションの手順を共通化するようコードをリファクタする(現在は以下モードの時のみ暗黙的にマイグレーションしてしまっている)
   if (["PERSONAL", "PERSONAL_SAMPLE"].includes(configs.GDASH_MODE)) {
     const command =
-      configs.GDASH_ENV === "dev"
+      configs.GDASH_ENV === "local"
         ? "pnpm --filter @git-dash/db db:migrate"
         : "npx @git-dash/db db:migrate";
-    execSync(`DB_FILE_NAME=${filePath} ${command}`);
+
+    try {
+      execSync(`DB_FILE_NAME=${filePath} ${command}`);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   const sqliteClient = createClient({ url: `file:${filePath}` });
