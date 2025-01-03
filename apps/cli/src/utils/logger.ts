5879fe50e7fd569e1703ab8f546404b1d24e4f00
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-03 12:31:01 +0900

    async logger

--- apps/cli/src/utils/logger.ts
+++ apps/cli/src/utils/logger.ts
@@ -14,9 +14,9 @@ const transports = pino.transport({
   ],
 });
 
-const destination = pino.destination({
-  sync: true,
-});
+// const destination = pino.destination({
+//   sync: true,
+// });
 
-export const logger = pino({ ...transports, ...destination });
+export const logger = pino(transports);
 logger.level = "trace";
