ea96cdc929dea1bce6c8d02cea4c447beaf82a81
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-04 12:02:11 +0900

    add note

--- apps/cli/src/utils/logger.ts
+++ apps/cli/src/utils/logger.ts
@@ -20,3 +20,10 @@ const transports = pino.transport({
 
 export const logger = pino(transports);
 logger.level = "trace";
+
+// NOTE:
+// ref: https://stackoverflow.com/a/70880356
+// ref: https://github.com/pinojs/pino/blob/main/docs/api.md#transport-object
+// > If the transport option is supplied to pino, a destination parameter may not also be passed as a separate argument to pino:
+// > pino({ transport: {}}, '/path/to/somewhere') // THIS WILL NOT WORK, DO NOT DO THIS
+// > pino({ transport: {}}, process.stderr) // THIS WILL NOT WORK, DO NOT DO THIS
