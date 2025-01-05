fbc1754759fa25cd98ec4d93e8c98f787ec501c2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-06 07:48:29 +0900

    fix

--- apps/web/app/routes/(login)/$workspaceId/settings/general/page.tsx
+++ apps/web/app/routes/(login)/$workspaceId/settings/general/page.tsx
@@ -1,7 +1,7 @@
 import { auth, hc } from "@/clients";
 import { Button } from "@/components/Button";
 import type { Route } from "@@/(login)/$workspaceId/settings/general/+types/page";
-import {  redirect } from "react-router";
+import { redirect } from "react-router";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   await auth.authStateReady();
@@ -11,16 +11,16 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   }
 
   const membersResponse = await hc.api.members[":workspaceId"].$get(
-      {
-        param: {
-          workspaceId: params.workspaceId,
-        },
+    {
+      param: {
+        workspaceId: params.workspaceId,
       },
-      {
-        headers: {
-          Authorization: `Bearer ${await auth.currentUser.getIdToken()}`,
-        },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${await auth.currentUser.getIdToken()}`,
       },
+    },
   );
 
   if (!membersResponse.ok) throw Error("Failed to fetch");
@@ -28,7 +28,7 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   const members = await membersResponse.json();
 
   const isMeOwner = members.some(
-      (m) => m.email === auth.currentUser?.email && m.role === "OWNER",
+    (m) => m.email === auth.currentUser?.email && m.role === "OWNER",
   );
 
   return {
@@ -37,23 +37,25 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
-  const {  isMeOwner } = loaderData;
+  const { isMeOwner } = loaderData;
 
   return (
-      <div>
-        <p className="text-gray-500">
-          Workspace owners can manage configs.
-        </p>
+    <div>
+      <p className="text-gray-500">Workspace owners can manage configs.</p>
 
-        <section className="flex justify-between mt-8">
-          <div>Manage Workspace Name</div>
-            <Button disabled={!isMeOwner} className="w-24">Update</Button>
-        </section>
+      <section className="flex justify-between mt-8">
+        <div>Manage Workspace Name</div>
+        <Button disabled={!isMeOwner} className="w-24">
+          Update
+        </Button>
+      </section>
 
-          <section className="flex justify-between mt-8">
-              <div>Delete Workspace</div>
-              <Button variant="destructive" disabled={!isMeOwner} className="w-24">Delete</Button>
-          </section>
-      </div>
+      <section className="flex justify-between mt-8">
+        <div>Delete Workspace</div>
+        <Button variant="destructive" disabled={!isMeOwner} className="w-24">
+          Delete
+        </Button>
+      </section>
+    </div>
   );
 }
