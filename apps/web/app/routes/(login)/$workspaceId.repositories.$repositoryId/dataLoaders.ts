6dc1456e6912e81bccc6ac77f69df90959b8fe29
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-05 09:37:24 +0900

    feat: add time to merge repo (#467)

--- apps/web/app/routes/(login)/$workspaceId.repositories.$repositoryId/dataLoaders.ts
+++ apps/web/app/routes/(login)/$workspaceId.repositories.$repositoryId/dataLoaders.ts
@@ -1,4 +1,8 @@
+import type { getWasmDb } from "@/clients";
 import { generateDailyData } from "@/lib/generateDailyData";
+import { prTbl } from "@git-dash/db";
+import { subDays } from "date-fns";
+import { and, eq, gte, isNotNull, not } from "drizzle-orm";
 
 type GraphData = {
   version: "0.1";
@@ -146,5 +150,129 @@ export const dataLoaderVulnerabilityLow = async (
       variance: 0.5,
       weekendReduction: false,
     }),
+  };
+};
+
+const renovateBotId = 29139614;
+
+export const dataLoaderTimeToMerge = async (
+  db: NonNullable<Awaited<ReturnType<typeof getWasmDb>>>,
+  repositoryId: number,
+) => {
+  const mergedPrs = await db
+    .select()
+    .from(prTbl)
+    .where(
+      and(
+        gte(prTbl.createdAt, subDays(new Date(), 60)),
+        eq(prTbl.repositoryId, repositoryId),
+        not(eq(prTbl.authorId, renovateBotId)),
+        isNotNull(prTbl.mergedAt),
+      ),
+    );
+  const mergedPrsWithMsec = mergedPrs.map((pr) => ({
+    ...pr,
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    elapsedMsec: pr.mergedAt!.getTime() - pr.createdAt.getTime(),
+  }));
+
+  const sumIn30Days = mergedPrsWithMsec
+    .filter((pr) => pr.createdAt > subDays(new Date(), 30))
+    .reduce((acc, pr) => acc + pr.elapsedMsec, 0);
+  const averageIn30Days = Math.round(
+    sumIn30Days /
+      mergedPrsWithMsec.filter((pr) => pr.createdAt > subDays(new Date(), 30))
+        .length,
+  );
+
+  const sumInPrevSpan = mergedPrsWithMsec
+    .filter(
+      (pr) =>
+        pr.createdAt <= subDays(new Date(), 30) &&
+        pr.createdAt > subDays(new Date(), 60),
+    )
+    .reduce((acc, pr) => acc + pr.elapsedMsec, 0);
+  const averageInPrevSpan = Math.round(
+    sumInPrevSpan /
+      mergedPrsWithMsec.filter(
+        (pr) =>
+          pr.createdAt <= subDays(new Date(), 30) &&
+          pr.createdAt > subDays(new Date(), 60),
+      ).length,
+  );
+
+  const aggregationBase = [
+    {
+      title: "0.5 day",
+      diffStart: 0,
+      diffEnd: 60 * 60 * 12 * 1000,
+      value: 0,
+      percentage: 0,
+      color: "bg-green-600 dark:bg-green-500",
+    },
+    {
+      title: "0.6~1 day",
+      diffStart: 60 * 60 * 12 * 1000,
+      diffEnd: 60 * 60 * 24 * 1000,
+      value: 0,
+      percentage: 0,
+      color: "bg-purple-600 dark:bg-purple-500",
+    },
+
+    {
+      title: "2~3 days",
+      diffStart: 60 * 60 * 24 * 1000,
+      diffEnd: 60 * 60 * 24 * 3 * 1000,
+      value: 0,
+      percentage: 0,
+      color: "bg-indigo-600 dark:bg-indigo-500",
+    },
+    {
+      title: "4~ days",
+      diffStart: 60 * 60 * 24 * 3 * 1000,
+      diffEnd: 60 * 60 * 24 * 60 * 1000,
+      value: 0,
+      percentage: 0,
+      color: "bg-gray-400 dark:bg-gray-600",
+    },
+  ];
+
+  const bars = aggregationBase.map((base) => ({
+    ...base,
+    value: `${
+      mergedPrsWithMsec
+        .filter((pr) => pr.createdAt > subDays(new Date(), 30))
+        .filter(
+          (msec) =>
+            msec.elapsedMsec >= base.diffStart &&
+            msec.elapsedMsec < base.diffEnd,
+        ).length
+    } PRs`,
+    percentage:
+      Math.round(
+        10 *
+          (mergedPrsWithMsec
+            .filter((pr) => pr.createdAt > subDays(new Date(), 30))
+            .filter(
+              (msec) =>
+                msec.elapsedMsec >= base.diffStart &&
+                msec.elapsedMsec < base.diffEnd,
+            ).length /
+            mergedPrsWithMsec.filter(
+              (pr) => pr.createdAt > subDays(new Date(), 30),
+            ).length) *
+          100,
+      ) / 10,
+  }));
+
+  return {
+    averageIn30Days,
+    averageInPrevSpan,
+    improvePercentage:
+      (averageInPrevSpan - averageIn30Days > 0 ? "+" : "") +
+      Math.round(
+        ((averageInPrevSpan - averageIn30Days) / averageInPrevSpan) * 100,
+      ),
+    bars,
   };
 };
