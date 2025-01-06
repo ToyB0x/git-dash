2cd2b817a7a95c9232bfc62c7863e86d37cb964f
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-07 04:55:57 +0900

    feat: aggregate repo by user type

--- apps/cli/src/commands/aggregate/repositories/aggregate.ts
+++ apps/cli/src/commands/aggregate/repositories/aggregate.ts
@@ -4,38 +4,78 @@ import { logger } from "@/utils";
 import { repositoryTbl } from "@git-dash/db";
 import { PromisePool } from "@supercharge/promise-pool";
 import { subDays } from "date-fns";
+import type { GetResponseDataTypeFromEndpointMethod } from "@octokit/types";
 
 export const aggregate = async (
+  userOrOrg: "User" | "Organization",
   octokit: Awaited<ReturnType<typeof getOctokit>>,
   sharedDbClient: ReturnType<typeof getDbClient>,
   configs: Configs,
   maxOldRepoDate?: Date,
 ) => {
-  // ref: https://docs.github.com/ja/rest/repos/repos?apiVersion=2022-11-28
-  const repos = await octokit.paginate(
-    octokit.rest.repos.listForOrg,
-    {
-      org: configs.GDASH_GITHUB_ORGANIZATION_NAME,
-      per_page: 100,
-      sort: "pushed",
-      direction: "desc",
-    },
-    (response, done) => {
-      const maxOld =
-        maxOldRepoDate ||
-        // NOTE: この部分でRepositoryが絞り込まれてしまうと、後続の脆弱性Alert検知Stepのリポジトリ等も絞り込されてしまう
-        // Googleであっても公開リポジトリは2700程度であり、100 * 27 = 27 point で取得できるので、リポジトリ自体は最初に全体を取得しても良いかもしれない
-        subDays(new Date(), configs.GDASH_COLLECT_DAYS_HEAVY_TYPE_ITEMS);
-      if (
-        response.data.find(
-          (repo) => !repo.pushed_at || new Date(repo.pushed_at) < maxOld,
-        )
-      ) {
-        done();
-      }
-      return response.data;
-    },
-  );
+  let repos:
+    | GetResponseDataTypeFromEndpointMethod<
+        typeof octokit.rest.repos.listForUser
+      >
+    | GetResponseDataTypeFromEndpointMethod<
+        typeof octokit.rest.repos.listForOrg
+      > = [];
+
+  if (userOrOrg === "User") {
+    // ref: https://docs.github.com/ja/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
+    repos = await octokit.paginate(
+      octokit.rest.repos.listForUser,
+      {
+        username: configs.GDASH_GITHUB_ORGANIZATION_NAME,
+        per_page: 100,
+        sort: "pushed",
+        direction: "desc",
+      },
+      (response, done) => {
+        const maxOld =
+          maxOldRepoDate ||
+          // NOTE: この部分でRepositoryが絞り込まれてしまうと、後続の脆弱性Alert検知Stepのリポジトリ等も絞り込されてしまう
+          // Googleであっても公開リポジトリは2700程度であり、100 * 27 = 27 point で取得できるので、リポジトリ自体は最初に全体を取得しても良いかもしれない
+          subDays(new Date(), configs.GDASH_COLLECT_DAYS_HEAVY_TYPE_ITEMS);
+        if (
+          response.data.find(
+            (repo) => !repo.pushed_at || new Date(repo.pushed_at) < maxOld,
+          )
+        ) {
+          done();
+        }
+        return response.data;
+      },
+    );
+  }
+
+  if (userOrOrg === "Organization") {
+    // ref: https://docs.github.com/ja/rest/repos/repos?apiVersion=2022-11-28
+    repos = await octokit.paginate(
+      octokit.rest.repos.listForOrg,
+      {
+        org: configs.GDASH_GITHUB_ORGANIZATION_NAME,
+        per_page: 100,
+        sort: "pushed",
+        direction: "desc",
+      },
+      (response, done) => {
+        const maxOld =
+          maxOldRepoDate ||
+          // NOTE: この部分でRepositoryが絞り込まれてしまうと、後続の脆弱性Alert検知Stepのリポジトリ等も絞り込されてしまう
+          // Googleであっても公開リポジトリは2700程度であり、100 * 27 = 27 point で取得できるので、リポジトリ自体は最初に全体を取得しても良いかもしれない
+          subDays(new Date(), configs.GDASH_COLLECT_DAYS_HEAVY_TYPE_ITEMS);
+        if (
+          response.data.find(
+            (repo) => !repo.pushed_at || new Date(repo.pushed_at) < maxOld,
+          )
+        ) {
+          done();
+        }
+        return response.data;
+      },
+    );
+  }
 
   const recentRepos = repos.filter((repo) => {
     const maxOld =
