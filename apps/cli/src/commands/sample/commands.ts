282283c6bea0a0501a646e42118ebd56bdf05bf7
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-07 05:27:40 +0900

    refactor: remove unused command (default / show command)

--- apps/cli/src/commands/sample/commands.ts
+++ apps/cli/src/commands/sample/commands.ts
@@ -1,28 +1,16 @@
 import { Command } from "commander";
-import { defaultCommand } from "./default";
 import { interactiveCommand } from "./interactive";
-import { showCommand } from "./show";
 
 export const newSampleCommand = () => {
   const sampleCmd = new Command("sample");
   sampleCmd.description("sample mode related commands.");
 
   sampleCmd
-    .command("*", { isDefault: true })
-    .description("aggregate with sample mode")
-    .action(async () => await defaultCommand());
-
-  sampleCmd
-    .command("interactive")
+    .command("interactive", { isDefault: true })
     .description(
       "Aggregate public or preferred organizational repositories using game-like points ui (only for fun, but you can use it for real)",
     )
     .action(async () => await interactiveCommand());
-
-  sampleCmd
-    .command("show")
-    .description("show sample mode aggregated result")
-    .action(async () => await showCommand());
 
   return sampleCmd;
 };
