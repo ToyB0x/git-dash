330ab3240c1cb313a1b6e5a492ea1a85671cf7f7
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-01-28 05:52:20 +0900

    feat: add console output for AI (#584)

--- apps/web/app/routes/(login)/$workspaceId.users.$userId/page.tsx
+++ apps/web/app/routes/(login)/$workspaceId.users.$userId/page.tsx
@@ -129,6 +129,63 @@ export default function Page({ loaderData, params }: Route.ComponentProps) {
     maxOldReview,
   } = loadData;
 
+  console.log(`以下は　ユーザ ${userName} の直近のGithubの活動データです。
+あなたは優しく経験豊富で前向きなAIのEMとして以下点を考慮しつつ、このユーザやチームにFBを伝えてください。
+
+- FBは直近7日分をスコープとする(さらにマージまでの時間やレビュー待ち時間は日本時間の土日や深夜早朝を考慮する)
+- 具体的なPRについて言及する場合はPRのNumberとタイトルに、GithubのPRに対するリンクをマークダウン的に含める形で記載してください (PRのowner/repository/numberを利用してGithubのPRのURLを合成して下さい)
+- このチームはGoogleが提唱するトランクベース開発を行っており、なるべく適切な粒度と頻度で細かくマージしていくことを目標としていることを考慮する
+- ある程度適切に目標達成できている場合は、必ずしも追加の改善は必要ないため「今回はいいペースで進んでいるのでこのペースを守りましょう」といったニュアンスのFBをする
+- 本人へのFBだけではなく、チームメンバーや人間のEMがこのユーザの開発生産性を向上させるために行動するためのヒントのセクションを設ける
+- スプリント最終日のレトロスペクティブの際にチームメンバーや人間のEMが振り返りのディスカッションをするための、レトロスペクティブの議題セクションを設ける
+- ワークライフバランスのセクションを追加した上で、活動内容のバランスが取れているかや危険な指標が出ていないがを確認してください
+
+--------------------------
+timeToMerge: ${JSON.stringify(timeToMerge, null, 2)},
+timeToReview: ${JSON.stringify(timeToReview, null, 2)},
+timeToReviewed: ${JSON.stringify(timeToReviewed, null, 2)},
+日中の活動(10時〜19時): ${
+    heatmap
+      .filter(({ count }) => count > 0)
+      .filter(({ time }) => subDays(new Date(), 7) < time)
+      .filter(({ time }) => 10 <= time.getHours() && time.getHours() <= 19)
+      .length
+  } 件,
+深夜早朝の活動(10時〜19時以外ではあるが社内規定の通常就業時間内): ${
+    heatmap
+      .filter(({ count }) => count > 0)
+      .filter(({ time }) => subDays(new Date(), 7) < time)
+      .filter(({ time }) => time.getHours() < 10 || 19 < time.getHours()).length
+  } 件,
+深夜早朝の残業(朝5時前、または夜22時以降): ${
+    heatmap
+      .filter(({ count }) => count > 0)
+      .filter(({ time }) => subDays(new Date(), 7) < time)
+      .filter(({ time }) => time.getHours() < 5 || 22 < time.getHours()).length
+  } 件,
+recentPrs: ${JSON.stringify(recentPrs, null, 2)},
+--------------------------
+`);
+
+  console.log(`以下は　ユーザ ${userName} の直近のGithubの活動データです。
+このユーザが属するチームは毎日14時頃にデイリースタンドアップMTGを開催しています。
+このユーザが今日のデイリーMTGで共有すべき内容を簡単にまとめてください。
+
+- 以下のrecentPrsを利用した上で、日本の土日祝日が考慮された、直近の１営業日以内のデータのみを利用してください (日時はrecentPrsのlastActivityのフィールドを利用してください)
+- 具体的なPRについて言及する場合はPRのNumberとタイトルに、GithubのPRに対するリンクをマークダウン的に含める形で記載してください (PRのowner/repository/numberを利用してGithubのPRのURLを合成して下さい)
+- やったこと、今日の予定、困っていることの3つのセクションを作ってください
+
+--------------------------
+recentPrs: ${JSON.stringify(
+    recentPrs.filter(
+      ({ lastActivity }) => subDays(new Date(), 4) < lastActivity,
+    ),
+    null,
+    2,
+  )},
+--------------------------
+${" ".repeat(10000)}`);
+
   const maxDate = startOfToday();
   const [selectedDates, setSelectedDates] = useState<DateRange | undefined>({
     from: subDays(maxDate, 30),
